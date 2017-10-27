44c9004090fb5f3fec9a199b9e586d0c8740a83e
mark liu <liuziyuan.room@gmail.com>
2017-10-27 11:50:39 +0800

    add auction history model

---
+++ app/models/auction_history.rb
@@ -0,0 +1,23 @@
+class AuctionHistory < ApplicationRecord
+  # Extends
+
+  # Includes
+
+  # Associations
+  belongs_to :user
+  belongs_to :auction
+
+  # accepts_nested_attributes
+
+  # Validations
+
+  # Scopes
+
+  # Callbacks
+
+  # Delegates
+
+  # Custom
+
+  # Methods (class methods before instance methods)
+end
