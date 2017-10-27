10234c35bd41212271f7e57669f01b1b3b2afbac
mark liu <liuziyuan.room@gmail.com>
2017-10-27 15:19:00 +0800

    add login

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,9 @@
 class Admin::BaseController < ApplicationController
-  before_action :authenticate_user!
   before_action :admin_required
 
   layout 'admin'
 
   private
-
   def admin_required
     head :unauthorized unless current_user && current_user.has_role?(:admin)
   end
