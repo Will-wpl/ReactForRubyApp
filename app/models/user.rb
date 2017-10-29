e274787c6343f9e2dcdbb00efc8af3adbca81f3d
mark liu <liuziyuan.room@gmail.com>
2017-10-29 16:36:50 +0800

    remove user_role associations and create the retailer controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   # Includes
 
   # Associations
-  has_and_belongs_to_many :roles, join_table: :users_roles
+  # has_and_belongs_to_many :roles, join_table: :users_roles
   has_many :arrangements
   has_many :auctions , :through => :arrangements
 
