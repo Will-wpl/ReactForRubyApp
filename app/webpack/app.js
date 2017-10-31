2e6849f896fd26d85d442d9db145a5fd7f576667
mark liu <liuziyuan.room@gmail.com>
2017-10-31 15:34:51 +0800

    retailer manage coming

--- app/webpack/app.js
+++ app/webpack/app.js
@@ -13,6 +13,7 @@ import "./styles/app.scss";
 
 import "components/hello-world";
 import "components/chat-channel";
+import "components/retailer-manage-coming";
 
 ActionCable = require('actioncable')
 
