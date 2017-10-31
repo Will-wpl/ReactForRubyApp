40e6c4453f2a43cb306dcb07ca6e8088da35441c
mark liu <liuziyuan.room@gmail.com>
2017-10-31 19:23:55 +0800

    add spec

--- app/controllers/admin/auctions_controller.rb
+++ app/controllers/admin/auctions_controller.rb
@@ -3,50 +3,76 @@ class Admin::AuctionsController < Admin::BaseController
 
   before_action :set_auctions_breadcrumbs
   before_action :set_action_breadcrumbs
-  # skip_before_action :verify_authenticity_token, :except => [:publish]
 
-  def index
-    @auctions = Auction.all.order(created_at: :desc).page(params[:page])
-  end
+  # def index
+  #   @auctions = Auction.all.order(created_at: :desc).page(params[:page])
+  # end
 
+  # GET create auction page
   def new
   end
 
-  # GET auction by AJAX
-  def getAuction
-    @auction = Auction.first
-    render json: @auction
+  # GET auction info by ajax
+  def obtain
+    if Auction.count == 0
+      render json: nil
+    else
+      @auction = Auction.first
+      render json: @auction, status: 200
+    end
+
   end
 
-  def create
-    @auction = Auction.new(model_params)
-
-    if @auction.save
-      redirect_to [:admin, @auction], notice: "#{Auction.model_name.human} was successfully created."
+  # POST saveOrUpdate auction info by ajax
+  def save
+    # @auction = model_params['id'] == nil ? Auction.new(model_params) : Auction.update(model_params)
+    if model_params['id'] == nil
+      @auction = Auction.new(model_params)
+      if @auction.save
+        render json: @auction, status: 201
+      else
+        render json: 'error code ', status: 500
+      end
     else
-      render :new
+      @auction = Auction.find(model_params['id'])
+      if @auction.update(model_params)
+        render json: @auction ,status: 201
+      else
+        render json: 'error code ', status: 500
+      end
     end
   end
 
-  def show
-  end
+  # default create
+  # def create
+  #   @auction = Auction.new(model_params)
 
-  def edit
-  end
+  #   if @auction.save
+  #     redirect_to [:admin, @auction], notice: "#{Auction.model_name.human} was successfully created."
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def update
-    if @auction.update(model_params)
-      redirect_to [:admin, @auction], notice: "#{Auction.model_name.human} was successfully updated."
-    else
-      render :edit
-    end
-  end
+  # def show
+  # end
 
-  def destroy
-    @auction.destroy
+  # def edit
+  # end
 
-    redirect_to admin_auctions_path, notice: "#{Auction.model_name.human} was successfully destroyed."
-  end
+  # def update
+  #   if @auction.update(model_params)
+  #     redirect_to [:admin, @auction], notice: "#{Auction.model_name.human} was successfully updated."
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  # def destroy
+  #   @auction.destroy
+
+  #   redirect_to admin_auctions_path, notice: "#{Auction.model_name.human} was successfully destroyed."
+  # end
 
   # GET upcoming page
   def upcoming
@@ -58,13 +84,13 @@ class Admin::AuctionsController < Admin::BaseController
 
   # GET dashbard page
   def dashboard
-    
+
   end
 
   # POST publish auction
   def publish
     @hello = params['hello']
-    render json: @hello , status: 200
+    render json: @hello, status: 200
 
   end
 
@@ -74,7 +100,7 @@ class Admin::AuctionsController < Admin::BaseController
 
   # POST extend time
   def delay
-    
+
   end
 
   # POST comfirm
@@ -83,17 +109,17 @@ class Admin::AuctionsController < Admin::BaseController
 
   private
 
-    def set_auction
-      @auction = Auction.find(params[:id])
-    end
+  def set_auction
+    @auction = Auction.find(params[:id])
+  end
 
-    def model_params
-      params.require(:auction).permit(:name, :start_datetime, :contract_period_start_date, :contract_period_end_date, :duration, :reserve_price)
-    end
+  def model_params
+    params.require(:auction).permit(:name, :start_datetime, :contract_period_start_date, :contract_period_end_date, :duration, :reserve_price)
+  end
 
-    def set_auctions_breadcrumbs
-      add_breadcrumb 'Home', admin_home_index_path
-      add_breadcrumb Auction.model_name.human.pluralize, admin_auctions_path
-      add_breadcrumb @auction.name_was, admin_auction_path(@auction) if @auction.try(:persisted?)
-    end
+  def set_auctions_breadcrumbs
+    add_breadcrumb 'Home', admin_home_index_path
+    add_breadcrumb Auction.model_name.human.pluralize, admin_auctions_path
+    add_breadcrumb @auction.name_was, admin_auction_path(@auction) if @auction.try(:persisted?)
+  end
 end
