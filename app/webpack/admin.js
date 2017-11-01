b8ae44fef5ff193ea5b578300a4d6ff1c4e46291
mark liu <liuziyuan.room@gmail.com>
2017-11-01 12:24:26 +0800

    整理组件文件夹结构

--- app/webpack/admin.js
+++ app/webpack/admin.js
@@ -2,7 +2,7 @@ import "atlas/src/index.js";
 
 import "./javascripts/shared.js";
 import "./styles/admin.scss";
-import "components/create-NewRa";
+import "components/admin/create-NewRa";
 
 console.log("Admin Webpacker");
 
