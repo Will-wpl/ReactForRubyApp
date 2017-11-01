5211b2e3c274d269470eb95a4c825d78790c64db
mark liu <liuziyuan.room@gmail.com>
2017-11-01 12:24:26 +0800

    整理组件文件夹结构

--- app/webpack/components/retailers/retailer-manage-coming.jsx
+++ app/webpack/components/retailers/retailer-manage-coming.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom';
-import {TimeCuntDown} from './time-cuntdown';
+import {TimeCuntDown} from '../shared/time-cuntdown';
 
 export class RetailerManage extends Component {
     constructor(props){
