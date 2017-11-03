000f16189fd7badb625d49805c982e673230e703
will <Will.wang@chinasofti.com>
2017-11-03 10:36:58 +0800

    make page of admin_online_ra and admin_dashboard for chars

--- app/webpack/components/common/chart/list-checkbox-item.js
+++ app/webpack/components/common/chart/list-checkbox-item.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import selected from '../../../selected.png';
-// import unselected from '../../../unselected.png';
+import selected from '../../../images/selected.png';
+import unselected from '../../../images/unselected.png';
 
 export default class CheckboxListItem extends Component {
 
@@ -17,9 +17,9 @@ export default class CheckboxListItem extends Component {
     }
     render() {
         return (
-            <li onClick={this.onItemClick.bind(this, this.props.id, !this.state.status, this.props.color)} style={{ color:this.props.color,cursor: 'pointer' }}>
-                {/*<input type="checkbox" onChange={this.onItemChecked.bind(this, this.props.id)} />*/}
-                {/*<img src={this.state.status ? selected : unselected} alt="selectstatus" />*/}
+            <li className="checkitem" onClick={this.onItemClick.bind(this, this.props.id, !this.state.status, this.props.color)} style={{ color:this.props.color,cursor: 'pointer' }}>
+                {/* <input type="checkbox" onChange={this.onItemChecked.bind(this, this.props.id)} /> */}
+                <img src={this.state.status ? selected : unselected} alt="selectstatus" />
                 <span>{this.props.display}  {this.props.id}</span>
             </li>
         );
