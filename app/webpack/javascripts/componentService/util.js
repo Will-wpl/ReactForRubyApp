3c22ab1fd27f6d218d51653a458b2789eb800ae0
huangsonglu <jason.huang@chinasofti.com>
2017-11-07 14:49:34 +0800

    get retailer history data in component

--- app/webpack/javascripts/componentService/util.js
+++ app/webpack/javascripts/componentService/util.js
@@ -9,3 +9,11 @@ export const getRandomColor = (intNum) => {
     // return '#' + ('00000' + (intNum * 0x1000000 << 0).toString(16)).slice(-6);
     return `#${(`00000${((Math.random() + intNum) * 0x1000000 << 0).toString(16)}`).slice(-6)}`;
 }
+
+export const getLoginUserId = () => {
+    let element = document.getElementById('user_id');
+    if (element) {
+        return element.getAttribute('value');
+    }
+    return 0;
+}
