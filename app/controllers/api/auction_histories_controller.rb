017c30bfcd29acbd8cb1ede508387c270e88bd8c
mark liu <liuziyuan.room@gmail.com>
2017-11-08 17:09:04 +0800

    modify get user when got histories

--- app/controllers/api/auction_histories_controller.rb
+++ app/controllers/api/auction_histories_controller.rb
@@ -9,11 +9,12 @@ class Api::AuctionHistoriesController < ApplicationController
   # get all users history list by auction_id
   def list
     @histories = AuctionHistory.select('users.name, users.company_name, auction_histories.*').joins(:user).where('auction_id = ?', params[:auction_id]).order(bid_time: :asc)
-    @users = Auction.find(params[:auction_id]).users
+    # @users = Auction.find(params[:auction_id]).users
+    @arrangements = Arrangement.where('auction_id = ? and accept_status = 1')
     hash = Hash.new
     list = Array.new
-    @users.each do |user|
-      hash[user.id] = Array.new
+    @arrangements.each do |arrangement|
+      hash[arrangement.user_id] = Array.new
     end
     @histories.each do |history|
       hash[history.user_id].push(history)
@@ -27,6 +28,10 @@ class Api::AuctionHistoriesController < ApplicationController
     end
 
     render json: list, status: 200
+  end
+
+  def winner
+
   end
 
 end
