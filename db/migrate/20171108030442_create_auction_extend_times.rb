4b62389f494fc8c331f6aada5762918ca503ad97
mark liu <liuziyuan.room@gmail.com>
2017-11-08 11:12:43 +0800

    modify auction extend time field

--- db/migrate/20171108030442_create_auction_extend_times.rb
+++ db/migrate/20171108030442_create_auction_extend_times.rb
@@ -1,7 +1,7 @@
 class CreateAuctionExtendTimes < ActiveRecord::Migration[5.1]
   def change
     create_table :auction_extend_times do |t|
-      t.datetime :extend_time
+      t.integer :extend_time
       t.datetime :current_time
       t.datetime :actual_begin_time
       t.datetime :actual_end_time
