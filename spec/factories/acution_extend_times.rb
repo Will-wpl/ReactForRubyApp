01b0636b95c72cae43046f9a1dc011eae300c301
mark liu <liuziyuan.room@gmail.com>
2017-11-08 11:01:50 +0800

    Revert "add auction extend time table"
    This reverts commit 2733cfaed2ae672614085572b73eeb969559ca1e.

--- spec/factories/acution_extend_times.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :acution_extend_time do
-    extend_time "2017-11-08 10:57:32"
-    current_time "2017-11-08 10:57:32"
-    actual_begin_time "2017-11-08 10:57:32"
-    actual_end_time "2017-11-08 10:57:32"
-  end
-end
