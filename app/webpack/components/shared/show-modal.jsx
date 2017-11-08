1d307579e6a8a421e993d8897631f456aca7d997
will <Will.wang@chinasofti.com>
2017-11-08 11:40:42 +0800

    show detail list

--- app/webpack/components/shared/show-modal.jsx
+++ app/webpack/components/shared/show-modal.jsx
@@ -5,7 +5,8 @@ export class Modal extends React.Component{
         super(props);
         this.state={
             modalshowhide:"modal_hide",
-            type:'default'
+            type:'default',
+            secondStatus:"live_hide"
         }
     }
     showModal(type){
@@ -33,17 +34,44 @@ export class Modal extends React.Component{
             modalshowhide:"modal_hide"
         })
     }
+    componentDidMount() {
+         
+    }
     render(){
-        let showDetail = '';
-        if(this.props.showdetail){
+        let showDetail = '',secondary = '',secondStatus='';
+        if(this.props.showdetail && !this.props.text){
+            this.secondaryData = [
+                this.props.showdetail.alternative_name,
+                this.props.showdetail.alternative_email_address,
+                this.props.showdetail.alternative_mobile_number,
+                this.props.showdetail.alternative_office_number
+            ]
+            let isChanged = this.secondaryData.some((element, index) => {
+                    return element !== '' || undefined;
+                })
+                if(isChanged){
+                    secondStatus = "live_show"
+                }else{
+                    secondStatus = "live_hide"
+                }     
+            secondary = <ol className={"showdetail "+secondStatus}>
+                                <li>Alternative Contact</li>
+                                <li>Name : {this.props.showdetail.alternative_name}</li>
+                                <li>Email : {this.props.showdetail.alternative_email_address}</li>
+                                <li>Mobile : {this.props.showdetail.alternative_mobile_number}</li>
+                                <li>Office : {this.props.showdetail.alternative_office_number}</li>
+                            </ol>
             showDetail = <ul className="showdetail">
+                            <li>Main Contact</li>
                             <li>Name : {this.props.showdetail.main_name}</li>
                             <li>Email : {this.props.showdetail.main_email_address}</li>
                             <li>Mobile : {this.props.showdetail.main_mobile_number}</li>
                             <li>Office : {this.props.showdetail.main_office_number}</li>
+                            {secondary}
                         </ul>
-        }
-        
+        }else{
+            showDetail = '';
+        }       
         let btn_html = '';
         if(this.state.type == "default"){
             btn_html = <div className="modal_btn"><a onClick={this.closeModal.bind(this)}>OK</a></div>;
