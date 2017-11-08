338c5585d0de78e95828cd81fd58542c2aea21fb
mark liu <liuziyuan.room@gmail.com>
2017-11-08 11:01:50 +0800

    add auction extend time table

---
+++ app/models/acution_extend_time.rb
@@ -0,0 +1,22 @@
+class AcutionExtendTime < ApplicationRecord
+  # Extends
+
+  # Includes
+
+  # Associations
+  belongs_to :user
+  belongs_to :auction
+  # accepts_nested_attributes
+
+  # Validations
+
+  # Scopes
+
+  # Callbacks
+
+  # Delegates
+
+  # Custom
+
+  # Methods (class methods before instance methods)
+end
