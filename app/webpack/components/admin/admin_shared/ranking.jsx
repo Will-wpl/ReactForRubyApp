0b134c4aa792a5b0910834cecc4d0a38177cf39d
huangsonglu <jason.huang@chinasofti.com>
2017-11-09 16:40:26 +0800

    fit ranking data with realtime data

--- app/webpack/components/admin/admin_shared/ranking.jsx
+++ app/webpack/components/admin/admin_shared/ranking.jsx
@@ -2,81 +2,54 @@ import React, {Component} from 'react';
 
 export default class RetailerRanking extends Component {
     render() {
-        console.log('realtimeRanking', this.props.ranking);
+        console.log('ranking', this.props.ranking)
         return (
             <div className="retailrank_main">
                 <h3>Retailer Ranking</h3>
                 <div className="retailrank_box">
-                    <table className="retailer_fill" cellPadding="0" cellSpacing="0">
-                        <thead>
-                        <tr>
-                            <th>Rank</th>
-                            <th>Retailer</th>
-                            <th>Price</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.props.ranking.map((item, index) => {
-                                return (
-                                    <tr key={item.id}>
-                                        <td>{index + 1}</td>
-                                        <td>{item.company_name}</td>
-                                        <td>{parseFloat(item.average_price).toFixed(4)}</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                        </tbody>
-                    </table>
+                    <div className="table-head">
+                        <table className="retailer_fill">
+                            <colgroup>
+                                <col style={{width: 50 + 'px'}}/>
+                                <col style={{width: 100 + 'px'}}/>
+                                <col style={{width: 100 + 'px'}}/>
+                            </colgroup>
+                            <thead>
+                            <tr>
+                                <th>Rank</th>
+                                <th>Retailer</th>
+                                <th>Price</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <div className="table-body">
+                        <table className="retailer_fill">
+                            <colgroup>
+                                <col style={{width: 50 + 'px'}}/>
+                                <col style={{width: 100 + 'px'}}/>
+                                <col style={{width: 100 + 'px'}}/>
+                            </colgroup>
+                            <tbody>
+                            {
+                                this.props.ranking.map((item, index) => {
+                                    return (
+                                        <tr key={item.id}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.company_name}</td>
+                                            <td>{parseFloat(item.average_price).toFixed(4)}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         )
     }
 }
-  render() {
-    return (
-      <div className="retailrank_main">
-      <h3>Retailer Ranking</h3>
-      <div className="retailrank_box">
-        <div className="table-head">
-      <table className="retailer_fill">
-                  <colgroup>  
-                        <col style={{width: 50 +'px'}} />  
-                        <col style={{width: 100 +'px'}} />
-                        <col style={{width: 100 +'px'}} />
-                    </colgroup> 
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Retailer</th>
-            <th>Price</th>
-            </tr>
-          </thead>
-      </table>
-      </div>
-      <div className="table-body">
-      <table className="retailer_fill">
-          <colgroup>  
-                        <col style={{width: 50 +'px'}} />  
-                        <col style={{width: 100 +'px'}} />
-                        <col style={{width: 100 +'px'}} />
-          </colgroup> 
-            <tbody>
-            {
-              this.state.list_data.map((item,index)=>{
-                return(
-                  <tr key={index}><td>{index+1}</td><td>{item.name}</td><td>{item.price}</td></tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-        </div>
-      </div>
-      </div>
-    )
-  }
 
 RetailerRanking.defaultProps = {
     ranking: []
