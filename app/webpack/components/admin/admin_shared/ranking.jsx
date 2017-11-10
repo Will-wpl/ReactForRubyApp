c2385122ed7fd8a299ac257522dabd164fea5212
huangsonglu <jason.huang@chinasofti.com>
2017-11-10 14:14:14 +0800

    trigger submit disable when time below 60 second

--- app/webpack/components/admin/admin_shared/ranking.jsx
+++ app/webpack/components/admin/admin_shared/ranking.jsx
@@ -4,7 +4,7 @@ export default class RetailerRanking extends Component {
     render() {
         console.log('ranking', this.props.ranking)
         this.props.ranking.sort((a, b) => {
-            return parseFloat(a.average_price) < parseFloat(b.average_price)
+            return parseFloat(a.average_price) > parseFloat(b.average_price)
         })
         return (
             <div className="retailrank_main">
