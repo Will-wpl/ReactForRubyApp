5ae121bd362cee182c1fc25bd3be75d451fff1b0
Brian <yangqingxin83@163.com>
2017-11-10 21:32:44 +0800

    Update fields and labels in admin confirm winner page

--- app/webpack/components/admin/admin_shared/winner.jsx
+++ app/webpack/components/admin/admin_shared/winner.jsx
@@ -18,16 +18,16 @@ export default class WinnerPrice extends Component {
         <h2 className={this.props.showOrhide}><span className={this.props.statusColor}>Status : {this.props.showStatus}</span></h2>
         <h4 className="u-mt1">Summary of Lowest Bidder</h4>
         <ul>
-          <li><span>Lowest Price Bidder : </span><span>{this.props.winnerData.company_name}</span></li>
-          <li><span>Winning Price : </span><span>{Number(this.props.winnerData.average_price).toFixed(4)}</span></li>
+          <li><span>Lowest Price Bidder: </span><span>{this.props.winnerData.company_name}</span></li>
+          <li><span>Lowest Average Price: </span><span>{Number(this.props.winnerData.average_price).toFixed(4)}</span></li>
         </ul>
         <table className="retailer_fill" cellPadding="0" cellSpacing="0">
             <thead>
             <tr>
                 <th></th>
                 <th>LT</th>
-                <th>HT(Small)</th>
-                <th>HT(Large)</th>
+                <th>HT (Small)</th>
+                <th>HT (Large)</th>
                 </tr>
             </thead>
             <tbody>
@@ -36,9 +36,9 @@ export default class WinnerPrice extends Component {
             </tbody>
       </table>
         <ul>
-          <li><span>Total Consumption Forecast : </span><span>{this.props.winnerData.total_award_sum} kWh</span></li>
-          <li><span>Contact Period : </span><span>{moment(this.props.winnerData.updated_at).format('LLL')}</span></li>
-          <li><span>Total Award Sum : </span><span>{this.props.winnerData.total_award_sum}</span></li>
+          <li><span>Contact Period: </span><span>{moment(this.props.winnerData.updated_at).format('LLL')}</span></li>
+          <li><span>Total Volume: </span><span>{this.props.winnerData.total_volume} kWh</span><span> (forecasted)</span></li>
+          <li><span>Total Award Sum : </span><span>{this.props.winnerData.total_award_sum}</span><span> (forecasted)</span></li>
           </ul>
       </div>
     )
