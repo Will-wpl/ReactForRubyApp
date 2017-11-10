7a92daaa210f1a3ad0c103c5bd37627778affc8d
huangsonglu <jason.huang@chinasofti.com>
2017-11-10 09:57:01 +0800

    matched reverse price

--- app/webpack/components/admin/admin_shared/reserveprice.jsx
+++ app/webpack/components/admin/admin_shared/reserveprice.jsx
@@ -2,14 +2,15 @@ import React, {Component} from 'react';
 
 export default class ReservePrice extends Component {
     render() {
-        let compareResult = parseFloat(this.props.realtimePrice) > parseFloat(this.props.price)
+        let matched = parseFloat(this.props.realtimePrice) < parseFloat(this.props.price);
+        console.log(this.props.realtimePrice, this.props.price)
         return (
             <dl className="reservePrice">
                 <dd>SP Group Reserve Auction</dd>
                 <dd>
                     <span>Reserve Price = $ {this.props.price} /KWh</span>
-                    <span className={compareResult ? 'success' : 'fail'}>
-                {compareResult ? 'Reserve Price Achieved' : 'Reserve Price Not Achieved'}
+                    <span className={matched ? 'success' : 'fail'}>
+                {matched ? 'Reserve Price Achieved' : 'Reserve Price Not Achieved'}
                 </span>
                 </dd>
             </dl>
