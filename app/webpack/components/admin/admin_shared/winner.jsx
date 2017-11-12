6f4b14d6e0876ff147a2e7d4f01fd79e2cad46e7
will <Will.wang@chinasofti.com>
2017-11-12 14:30:46 +0800

    admin confirm winner

--- app/webpack/components/admin/admin_shared/winner.jsx
+++ app/webpack/components/admin/admin_shared/winner.jsx
@@ -18,8 +18,8 @@ export default class WinnerPrice extends Component {
         <h2 className={this.props.showOrhide}><span className={this.props.statusColor}>Status : {this.props.showStatus}</span></h2>
         <h4 className="u-mt1">Summary of Lowest Bidder</h4>
         <ul>
-          <li><span>Lowest Price Bidder: </span><span>{this.props.winner.company_name}</span></li>
-          <li><span>Lowest Average Price: </span><span>{Number(this.props.winner.average_price).toFixed(4)}</span></li>
+          <li><span>Lowest Price Bidder: </span><span>{this.props.winner.data.company_name}</span></li>
+          <li><span>Lowest Average Price: </span><span>{Number(this.props.winner.data.average_price).toFixed(4)}</span></li>
         </ul>
         <table className="retailer_fill" cellPadding="0" cellSpacing="0">
             <thead>
@@ -31,14 +31,14 @@ export default class WinnerPrice extends Component {
                 </tr>
             </thead>
             <tbody>
-                <tr><td>Peak (7am-7pm)</td><td>{this.props.winner.lt_peak}</td><td>{this.props.winner.hts_peak}</td><td>{this.props.winner.htl_peak}</td></tr>
-                <tr><td>Off-Peak (7pm-7am)</td><td>{this.props.winner.lt_off_peak}</td><td>{this.props.winner.hts_off_peak}</td><td>{this.props.winner.htl_off_peak}</td></tr>
+                <tr><td>Peak (7am-7pm)</td><td>{this.props.winner.data.lt_peak}</td><td>{this.props.winner.data.hts_peak}</td><td>{this.props.winner.data.htl_peak}</td></tr>
+                <tr><td>Off-Peak (7pm-7am)</td><td>{this.props.winner.data.lt_off_peak}</td><td>{this.props.winner.data.hts_off_peak}</td><td>{this.props.winner.data.htl_off_peak}</td></tr>
             </tbody>
       </table>
         <ul>
-          <li><span>Contact Period: </span><span>{moment(this.props.winnerAuction.contract_period_start_date).format('LL')} to {moment(this.props.winnerAuction.contract_period_end_date).format('LL')}</span></li>
-          <li><span>Total Volume: </span><span>{this.props.winnerAuction.total_volume} kWh</span><span></span></li>
-          <li><span>Total Award Sum: </span><span>{this.props.winner.total_award_sum}</span><span></span></li>
+          <li><span>Contact Period: </span><span>{moment(this.props.winner.auction.contract_period_start_date).format('LL')} to {moment(this.props.winner.auction.contract_period_end_date).format('LL')}</span></li>
+          <li><span>Total Volume: </span><span>{this.props.winner.auction.total_volume} kWh</span><span></span></li>
+          <li><span>Total Award Sum: </span><span>{this.props.winner.data.total_award_sum}</span><span></span></li>
           </ul>
       </div>
     ):(
