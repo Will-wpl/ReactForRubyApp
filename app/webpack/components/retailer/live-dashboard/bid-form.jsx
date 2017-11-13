dd13099467961b18a87548d1ee824aa1937ead72
huangsonglu <jason.huang@chinasofti.com>
2017-11-13 16:42:43 +0800

    bid form reformat

--- app/webpack/components/retailer/live-dashboard/bid-form.jsx
+++ app/webpack/components/retailer/live-dashboard/bid-form.jsx
@@ -7,115 +7,145 @@ export default class BidForm extends Component {
         this.state = {
             configs: ['0000', '0000', '0000', '0000', '0000', '0000'],
             changed: true,
-            error:false,
-            thisStatus:false
+            samePrice: false,
+            thisStatus: false,
+            status: [true, true, true, true, true, true]
         }
     }
 
     componentWillReceiveProps(nextProps) {
-        this.setState({configs:nextProps.data.map(element => {
-            return parseFloat(element).toFixed(4).substring(2);
-        })});
+        this.setState({
+            configs: nextProps.data.map(element => {
+                return parseFloat(element).toFixed(4).substring(2);
+            }), status: this.state.status.map(element => {
+                return true;
+            })
+        });
     }
 
     onInputChanged(i, e) {
         let num = parseInt(e.target.value);
         if (num < 10000) {
             if (num > 0) {
-                num = parseFloat(num * 1.0 /10000).toFixed(4).substring(2);
+                num = parseFloat(num * 1.0 / 10000).toFixed(4).substring(2);
             } else {
                 num = '0000';
             }
             let target = this.props.data.find((element, index) => {
                 return index === i;
             });
-            if (Number(num) > Number(target) * 10000) {
-                console.log('not allow', this.props.data);
+            let status = this.state.status;
+            if (Number(num) * 1.0 / 10000 > Number(target)) {
+                status[i] = false;
             } else {
-                this.setState({configs: this.state.configs.map((element, index) => {
+                status[i] = true;
+            }
+            this.setState({
+                configs: this.state.configs.map((element, index) => {
                     if (index === i) {
                         element = num;
                     }
                     return element;
-                })});
-            }
+                }), status: status
+            });
         }
     }
 
     onSubmit() {
         this.setState({
-            thisStatus:true
+            thisStatus: true
         })
-        console.log(this.state.configs);
         let isChanged = this.state.configs.some((element, index) => {
             //console.log("state:"+Number(element) / 10000+"--------props:"+parseFloat(this.props.data[index]));
-            return Number(element) / 10000 !== parseFloat(this.props.data[index]);
+            return Number(element) / 10000 < Number(this.props.data[index]);
         })
-        console.log(isChanged);
         if (this.props.onSubmit && isChanged) {
             this.props.onSubmit(this.state.configs);
             this.setState({
-                error:false
+                samePrice: false
             })
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.setState({
-                    error:true,
-                    thisStatus:false
+                    samePrice: true,
+                    thisStatus: false
                 })
-            },5000)
-        }else{
+            }, 5000)
+        } else {
             this.setState({
-                error:true
+                samePrice: true
             })
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.setState({
-                    error:false,
-                    thisStatus:false
+                    samePrice: false,
+                    thisStatus: false
                 })
-            },5000)
+            }, 5000)
         }
     }
 
     render() {
-        let error_html = '';
-        if(this.state.thisStatus){
-            !this.state.error ? error_html =<div className="number_error">Your bid has been successfully submitted.</div> : error_html = <div className="number_error">Invalid submission. Please check that your bid submission fulfils the following criteria:<br/>
-                                                                                        1.None of the values should be higher than its previous value<br/>
-                                                                                        2.At least one of the values must be lower than its previous value</div>;
+        let error_html;
+        if (this.state.thisStatus) {
+            error_html = !this.state.samePrice ?
+                <div className="number_error">Your bid has been successfully submitted.</div>
+                : <div className="number_error">Invalid submission. Please check that your bid submission fulfils the
+                    following criteria:<br/>
+                    1.None of the values should be higher than its previous value<br/>
+                    2.At least one of the values must be lower than its previous value</div>;
         }
-        
+        let illegal;
+        if (this.state.status.some(element => {
+                return !element
+            })) {
+            illegal = <p className="number_error" style={{color: 'red'}}>need lesser</p>;
+        }
         return (
             <div className="number_form">
-            <form>
-                <h3>Enter My Bids</h3>
-                <table className="retailer_fill u-mt2">
-                    <thead>
-                    <tr>
-                        <th></th>
-                        <th>LT</th>
-                        <th>HT(Small)</th>
-                        <th>HT(Large)</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>Peak (7am-7pm)</td>
-                        <td>$0.<input type="number" value={this.state.configs[1]} onChange={this.onInputChanged.bind(this, 1)}/></td>
-                        <td>$0.<input type="number" value={this.state.configs[3]} onChange={this.onInputChanged.bind(this, 3)}/></td>
-                        <td>$0.<input type="number" value={this.state.configs[5]} onChange={this.onInputChanged.bind(this, 5)}/></td>
-                    </tr>
-                    <tr>
-                        <td>Off-Peak (7pm-7am)</td>
-                        <td>$0.<input type="number" value={this.state.configs[0]} onChange={this.onInputChanged.bind(this, 0)}/></td>
-                        <td>$0.<input type="number" value={this.state.configs[2]} onChange={this.onInputChanged.bind(this, 2)}/></td>
-                        <td>$0.<input type="number" value={this.state.configs[4]} onChange={this.onInputChanged.bind(this, 4)}/></td>
-                    </tr>
+                <form>
+                    <h3>Enter My Bids</h3>
+                    <table className="retailer_fill u-mt2">
+                        <thead>
+                        <tr>
+                            <th></th>
+                            <th>LT</th>
+                            <th>HT(Small)</th>
+                            <th>HT(Large)</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Peak (7am-7pm)</td>
+                            <td>$0.<input type="number" value={this.state.configs[1]}
+                                          style={{borderColor: this.state.status[1] ? 'white' : 'red'}}
+                                          onChange={this.onInputChanged.bind(this, 1)}/></td>
+                            <td>$0.<input type="number" value={this.state.configs[3]}
+                                          style={{borderColor: this.state.status[3] ? 'white' : 'red'}}
+                                          onChange={this.onInputChanged.bind(this, 3)}/></td>
+                            <td>$0.<input type="number" value={this.state.configs[5]}
+                                          style={{borderColor: this.state.status[5] ? 'white' : 'red'}}
+                                          onChange={this.onInputChanged.bind(this, 5)}/></td>
+                        </tr>
+                        <tr>
+                            <td>Off-Peak (7pm-7am)</td>
+                            <td>$0.<input type="number" value={this.state.configs[0]}
+                                          style={{borderColor: this.state.status[0] ? 'white' : 'red'}}
+                                          onChange={this.onInputChanged.bind(this, 0)}/></td>
+                            <td>$0.<input type="number" value={this.state.configs[2]}
+                                          style={{borderColor: this.state.status[2] ? 'white' : 'red'}}
+                                          onChange={this.onInputChanged.bind(this, 2)}/></td>
+                            <td>$0.<input type="number" value={this.state.configs[4]}
+                                          style={{borderColor: this.state.status[4] ? 'white' : 'red'}}
+                                          onChange={this.onInputChanged.bind(this, 4)}/></td>
+                        </tr>
 
-                    </tbody>
-                </table>
-                <button type="button" className="lm--button lm--button--primary u-mt2 fright" onClick={this.onSubmit.bind(this)}>Submit</button>
-                {error_html}
-            </form>
+                        </tbody>
+                    </table>
+                    {illegal}
+                    <button type="button" className="lm--button lm--button--primary u-mt2 fright"
+                            onClick={this.onSubmit.bind(this)}>Submit
+                    </button>
+                    {error_html}
+                </form>
             </div>
         );
     }
