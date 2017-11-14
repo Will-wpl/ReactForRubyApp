e8d86c5f22c1cc79eef3c4480cc05180557694dc
will <Will.wang@chinasofti.com>
2017-11-14 13:25:14 +0800

    admin edit logic

--- app/webpack/javascripts/shared.js
+++ app/webpack/javascripts/shared.js
@@ -4,22 +4,9 @@ import {getLoginUserId} from './componentService/util';
 import {logout} from './componentService/common/service';
 export default class UserLogout extends Component {
     componentDidMount(){
-        window.addEventListener("beforeunload", function(event) {
-            event.returnValue = "affaa";
-            console.log(event);
-            return;
-            let n = event.screenX - window.screenLeft;
-            let b = n > document.documentElement.scrollWidth -20;
-            if(b && event.clientY < -40 || event.altKey){
-                alert("关闭浏览器");
-                this.doOut();
-            }else if(event.clientY > -40 && (event.clientY < -30) && event.clientX > 500){
-                alert("关闭标签页");
-                this.doOut();
-            }else{
-                alert("刷新页面");
-            }
-        });
+        $(window).bind('beforeunload',function(){
+            return '您输入的内容尚未保存，确定离开此页面吗？';
+        });      
     }
     doOut(){
         let user_id = getLoginUserId();
