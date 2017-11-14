3abf112f5c00f1c39db08e29d4de6508047190f4
mark liu <liuziyuan.room@gmail.com>
2017-11-14 20:46:13 +0800

    add actual retailer infomation

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,141 @@ if User.count == 0
   Role.create(name: 'retailer')
   Role.create(name: 'buyer')
 
+  @retailer1 = User.create(name: 'Best Electricity Supply ',
+                           email: 'enquiry@bestelectricity.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'enquiry@bestelectricity.com.sg')
+  @retailer1.add_role :retailer
+
+  @retailer2 = User.create(name: 'Charis Electric',
+                           email: 'Kenneth.lee@chariselectric.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Charis Electric')
+  @retailer2.add_role :retailer
+
+  @retailer3 = User.create(name: 'Cleantech Solar Management Company',
+                           email: 'contact@cleantechsolar.com',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Cleantech Solar Management Company')
+  @retailer3.add_role :retailer
+
+  @retailer4 = User.create(name: 'Diamond Energy Merchants',
+                           email: 'dekay@diamond-energy.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Diamond Energy Merchants')
+  @retailer4.add_role :retailer
+
+  @retailer5 = User.create(name: 'Energy Supply Solutions',
+                           email: 'lim@energysupplysolutions.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Energy Supply Solutions')
+  @retailer5.add_role :retailer
+
+  @retailer6 = User.create(name: 'Environmental Solutions (Asia)',
+                           email: 'info@env-solutions.com',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Environmental Solutions (Asia)')
+  @retailer6.add_role :retailer
+
+  @retailer7 = User.create(name: 'Hyflux Energy',
+                           email: 'Huixin_tang@hyflux.com',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Hyflux Energy')
+  @retailer7.add_role :retailer
+
+  @retailer8 = User.create(name: 'I Switch',
+                           email: 'Senthil.kumar@iswitch.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'I Switch')
+  @retailer8.add_role :retailer
+
+  @retailer9 = User.create(name: 'Just Electric',
+                           email: 'jeffrey.ng@justelectric.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Just Electric')
+  @retailer9.add_role :retailer
+
+  @retailer10 = User.create(name: 'Keppel Electric',
+                           email: 'Peiling.liew@kepinfra.com',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Keppel Electric')
+  @retailer10.add_role :retailer
+
+  @retailer11 = User.create(name: 'Ohm Energy',
+                           email: 'info@burienergy.com',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Ohm Energy')
+  @retailer11.add_role :retailer
+
+  @retailer12 = User.create(name: 'PacificLight Energy',
+                           email: 'eugene.he@pacificlight.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'PacificLight Energy')
+  @retailer12.add_role :retailer
+
+  @retailer13 = User.create(name: 'Red Dot Power',
+                           email: 'hafizah.ahmad@reddotpower.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Red Dot Power')
+  @retailer13.add_role :retailer
+
+  @retailer14 = User.create(name: 'SembCorp Power',
+                           email: 'Jenny.lye@sembcorp.com',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'SembCorp Power')
+  @retailer14.add_role :retailer
+
+
+  @retailer15 = User.create(name: 'Senoko Energy Supply',
+                           email: 'dylanng@senokoenergy.com',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Senoko Energy Supply')
+  @retailer15.add_role :retailer
+
+  @retailer16 = User.create(name: 'Seraya Energy',
+                           email: 'lamkl@pseraya.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Seraya Energy')
+  @retailer16.add_role :retailer
+
+  @retailer17 = User.create(name: 'Sunseap Energy',
+                           email: 'Darius.kok@sunseap.com',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Sunseap Energy')
+  @retailer17.add_role :retailer
+
+  @retailer18 = User.create(name: 'Tuas Power Supply',
+                           email: 'angiebai@tuaspower.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Tuas Power Supply')
+  @retailer18.add_role :retailer
+
+  @retailer19 = User.create(name: 'Union Power',
+                           email: 'hermann@unionpower.com.sg',
+                           password: 'password',
+                           password_confirmation: 'password',
+                           company_name: 'Union Power')
+  @retailer19.add_role :retailer
+
+
   @mark = User.create(name: 'Keppel Electric Mark',
                       email: 'mark@example.com',
                       password: 'password',
@@ -44,6 +179,7 @@ if User.count == 0
                       password_confirmation: 'password',
                       company_name: 'Cleantech Solar Management Company Judy')
   @judy.add_role :retailer
+
 end
 
 if Auction.count == 0
@@ -65,6 +201,255 @@ end
 
 if Arrangement.count == 0
   Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 2,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 3,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 4,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 5,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 6,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 7,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 8,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 9,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 10,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 11,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 12,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 13,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 14,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 15,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 16,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 17,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 18,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 19,
+                     auction_id: 1)
+  Arrangement.create(main_name: '',
+                     main_email_address: '',
+                     main_mobile_number: '',
+                     main_office_number: '',
+                     lt_peak: nil,
+                     lt_off_peak: nil,
+                     hts_peak: nil,
+                     hts_off_peak: nil,
+                     htl_peak: nil,
+                     htl_off_peak: nil,
+                     accept_status: '2',
+                     user_id: 20,
+                     auction_id: 1)
+
+
+  Arrangement.create(main_name: '',
                      main_email_address: 'Mark1@123.com',
                      main_mobile_number: '12345678',
                      main_office_number: '12345678',
@@ -75,7 +460,7 @@ if Arrangement.count == 0
                      htl_peak: nil,
                      htl_off_peak: nil,
                      accept_status: '2',
-                     user_id: 2,
+                     user_id: 21,
                      auction_id: 1)
   Arrangement.create(main_name: '',
                      main_email_address: 'Brian2@123.com',
@@ -88,7 +473,7 @@ if Arrangement.count == 0
                      htl_peak: nil,
                      htl_off_peak: nil,
                      accept_status: '2',
-                     user_id: 3,
+                     user_id: 22,
                      auction_id: 1)
   Arrangement.create(main_name: '',
                      main_email_address: 'Jason3@163.com',
@@ -101,7 +486,7 @@ if Arrangement.count == 0
                      htl_peak: nil,
                      htl_off_peak: nil,
                      accept_status: '2',
-                     user_id: 4,
+                     user_id: 23,
                      auction_id: 1)
   Arrangement.create(main_name: '',
                      main_email_address: 'Will4@163.com',
@@ -114,7 +499,7 @@ if Arrangement.count == 0
                      htl_peak: nil,
                      htl_off_peak: nil,
                      accept_status: '2',
-                     user_id: 5,
+                     user_id: 24,
                      auction_id: 1)
   Arrangement.create(main_name: '',
                      main_email_address: 'Judy5@google.com',
@@ -127,6 +512,6 @@ if Arrangement.count == 0
                      htl_peak: nil,
                      htl_off_peak: nil,
                      accept_status: '2',
-                     user_id: 6,
+                     user_id: 25,
                      auction_id: 1)
 end
