5d7375031789123d499ebc9c9809e129954cc660
huangsonglu <jason.huang@chinasofti.com>
2017-11-15 11:39:43 +0800

    add default color before calculate random color

--- app/webpack/javascripts/componentService/util.js
+++ app/webpack/javascripts/componentService/util.js
@@ -10,8 +10,16 @@ export const findUpLimit = (curValue) => {
     return 1;
 }
 
-export const getRandomColor = (intNum) => {
+const defaultColors = ['#22ad38', '#ffff00', '#f53d0b', '#8ff830', '#f13de8', '#37b8ff', '#ffffff', '#ffc000'
+    , '#3366ff', '#9933ff', '#868686', '#0ba55c', '#fa9106', '#ffafff', '#c00000', '#46f0f0', '#49702e', '#ffff99'
+    , '#993300', '#8e8cf4']
+export const getRandomColor = (intNum, totallyRandom = false) => {
     // return '#' + ('00000' + (intNum * 0x1000000 << 0).toString(16)).slice(-6);
+    if (!totallyRandom) {
+        if (defaultColors.hasOwnProperty(intNum)) {
+            return totallyRandom[intNum];
+        }
+    }
     return `#${(`00000${((Math.random() + intNum) * 0x1000000 << 0).toString(16)}`).slice(-6)}`;
 }
 
