2cd8c45bcad5528493e1056b974b8c8dd948b37e
huangsonglu <jason.huang@chinasofti.com>
2017-11-16 10:26:52 +0800

    add yaxis scroll bar

--- app/webpack/components/common/chart/list-checkbox-item.js
+++ app/webpack/components/common/chart/list-checkbox-item.js
@@ -20,7 +20,7 @@ export default class CheckboxListItem extends Component {
             <li className="checkitem" onClick={this.onItemClick.bind(this, this.props.id, !this.state.status, this.props.color)} style={{ color:this.props.color,cursor: 'pointer' }}>
                 {/* <input type="checkbox" onChange={this.onItemChecked.bind(this, this.props.id)} /> */}
                 <img src={this.state.status ? selected : unselected} alt="selectstatus" />
-                <span>{this.props.display}  {this.props.id}</span>
+                <span>{this.props.display}</span>
             </li>
         );
     }
