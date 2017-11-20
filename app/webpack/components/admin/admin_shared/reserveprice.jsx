bf9f5fd8bd62ca2eaed362f8fe7342bfe85848a6
will <Will.wang@chinasofti.com>
2017-11-20 14:51:37 +0800

    empty page style reserveprice title winner add ',' for 3

--- app/webpack/components/admin/admin_shared/reserveprice.jsx
+++ app/webpack/components/admin/admin_shared/reserveprice.jsx
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
-
+import {getAuction} from '../../../javascripts/componentService/common/service';
 export default class ReservePrice extends Component {
+    componentDidMount() {
+        getAuction().then(auction => {
+            this.name = auction.name;
+        }, error => {
+        })
+    }
     render() {
         let matched
         if(this.props.realtimePrice){
@@ -11,7 +17,7 @@ export default class ReservePrice extends Component {
         // console.log(this.props.realtimePrice, this.props.price)
         return (
             <dl className="reservePrice">
-                <dd>SP Group Reverse Auction</dd>
+                <dd>{this.name}</dd>
                 <dd>
                     <span>Reserve Price = $ {this.props.price}/kWh</span>
                     <span className={matched ? 'success' : 'fail'}>
