7332abb51372636205aa54a906ca444a29072430
will <Will.wang@chinasofti.com>
2017-11-20 16:08:24 +0800

    logout fix

--- app/webpack/admin.js
+++ app/webpack/admin.js
@@ -1,6 +1,6 @@
 import "atlas/src/index.js";
 
-import "./javascripts/shared.js";
+//import "./javascripts/shared.js";
 import "./styles/admin.scss";
 import "components/admin/create-new-ra";
 import "components/admin/admin-manage-published-ra";
