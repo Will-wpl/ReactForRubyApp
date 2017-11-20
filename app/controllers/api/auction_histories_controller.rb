991a944b4b50be95a783379450a8c6d6e2e070ff
liuziyuan <liuziyuan.room@gmail.com>
2017-11-20 18:11:12 +0800

    change user_id to current_user.id

--- app/controllers/api/auction_histories_controller.rb
+++ app/controllers/api/auction_histories_controller.rb
@@ -2,7 +2,7 @@ class Api::AuctionHistoriesController < Api::BaseController
 
   # get histories by user_id and auction_id
   def show
-    @histories = AuctionHistory.select('users.name, users.company_name, auction_histories.*').joins(:user).where('auction_id = ? and user_id = ?', params[:auction_id], params[:user_id]).order(bid_time: :asc)
+    @histories = AuctionHistory.select('users.name, users.company_name, auction_histories.*').joins(:user).where('auction_id = ? and user_id = ?', params[:auction_id], current_user.id).order(bid_time: :asc)
     render json: @histories, status: 200
   end
 
