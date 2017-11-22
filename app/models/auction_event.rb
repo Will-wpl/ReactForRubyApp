3ff761ec15af15f8cc6e5c138efdd92842b7a9ca
Michael Cheng Chi Mun <MICHAELCCM@SINGAPOREPOWER.COM.SG>
2017-11-22 14:48:23 +0800

    model test with refactored codes for auction, auction_event,aunction_history and user extension.

--- app/models/auction_event.rb
+++ app/models/auction_event.rb
@@ -20,12 +20,12 @@ class AuctionEvent < ApplicationRecord
 
   # Methods (class methods before instance methods)
   def self.set_events(user_id, auction_id, action, what)
-    event = AuctionEvent.new
-    event.auction_when = Time.now
-    event.auction_do = action
-    event.auction_what = what
-    event.auction_id = auction_id
-    event.user_id = user_id
-    event.save
+    AuctionEvent.create(
+      auction_when: Time.now,
+      auction_do: action,
+      auction_what: what,
+      auction_id: auction_id,
+      user_id: user_id
+    )
   end
 end
