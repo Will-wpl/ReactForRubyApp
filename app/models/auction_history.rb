e9d3626e43e9fec81d668d4624b153df9999ef62
Michael Cheng Chi Mun <MICHAELCCM@SINGAPOREPOWER.COM.SG>
2017-11-22 16:43:56 +0800

    Refactor auction history

--- app/models/auction_history.rb
+++ app/models/auction_history.rb
@@ -14,6 +14,8 @@ class AuctionHistory < ApplicationRecord
   # Validations
 
   # Scopes
+  scope :find_bidder_histories, ->(auction_id) { where('auction_id = ? and is_bidder = true', auction_id) }
+  scope :user_auction_histories, ->(auction_id, user_id) { where('auction_id = ? and user_id = ?', auction_id, user_id) }
 
   # Callbacks
 
@@ -24,40 +26,45 @@ class AuctionHistory < ApplicationRecord
   # Methods (class methods before instance methods)
 
   def self.save_update_sort_init_auction_histories(calculate_dto)
-    @auction = Auction.find(calculate_dto.auction_id)
-    @histories = AuctionHistory.where('auction_id = ? and user_id = ?', calculate_dto.auction_id, calculate_dto.user_id)
-    total_volume = Auction.set_total_volume(
-      @auction.total_lt_peak, @auction.total_lt_off_peak,
-      @auction.total_hts_peak, @auction.total_hts_off_peak,
-      @auction.total_htl_peak, @auction.total_htl_off_peak
-    )
-    total_award_sum = set_total_award_sum(@auction.total_lt_peak, @auction.total_lt_off_peak,
-                                          @auction.total_hts_peak, @auction.total_hts_off_peak,
-                                          @auction.total_htl_peak, @auction.total_htl_off_peak,
-                                          calculate_dto.lt_peak, calculate_dto.lt_off_peak, calculate_dto.hts_peak, calculate_dto.hts_off_peak, calculate_dto.htl_peak, calculate_dto.htl_off_peak)
-    average_price = set_average_price(total_award_sum, total_volume)
-    if @histories.count == 0
-      @history = AuctionHistory.new(lt_peak: calculate_dto.lt_peak, lt_off_peak: calculate_dto.lt_off_peak, hts_peak: calculate_dto.hts_peak, hts_off_peak: calculate_dto.hts_off_peak, htl_peak: calculate_dto.htl_peak, htl_off_peak: calculate_dto.htl_off_peak, bid_time: @auction.actual_begin_time, actual_bid_time: @auction.actual_begin_time,
-                                    user_id: calculate_dto.user_id, auction_id: calculate_dto.auction_id, average_price: average_price, total_award_sum: total_award_sum, is_bidder: true)
-      if @history.save
-        find_sort_update_auction_histories(calculate_dto.auction_id)
-      end
-    else
-      @history = @histories.first
-      if @history.update(lt_peak: calculate_dto.lt_peak, lt_off_peak: calculate_dto.lt_off_peak, hts_peak: calculate_dto.hts_peak, hts_off_peak: calculate_dto.hts_off_peak, htl_peak: calculate_dto.htl_peak, htl_off_peak: calculate_dto.htl_off_peak, bid_time: @auction.actual_begin_time, actual_bid_time: @auction.actual_begin_time,
-                         user_id: calculate_dto.user_id, auction_id: calculate_dto.auction_id, average_price: average_price, total_award_sum: total_award_sum, is_bidder: true)
-        find_sort_update_auction_histories(calculate_dto.auction_id)
-      end
-    end
-  end
+    auction = Auction.find(calculate_dto.auction_id)
+    user_auction_histories = user_auction_histories(calculate_dto.auction_id, calculate_dto.user_id)
 
-  def self.find_bidder_histories(auction_id)
-    AuctionHistory.where('auction_id = ? and is_bidder = ?', auction_id, true)
+    total_award_sum = set_total_award_sum(
+      [auction.total_lt_peak, auction.total_lt_off_peak,
+       auction.total_hts_peak, auction.total_hts_off_peak,
+       auction.total_htl_peak, auction.total_htl_off_peak],
+      [calculate_dto.lt_peak, calculate_dto.lt_off_peak,
+       calculate_dto.hts_peak, calculate_dto.hts_off_peak,
+       calculate_dto.htl_peak, calculate_dto.htl_off_peak]
+    )
+    total_volume = Auction.set_total_volume(
+      auction.total_lt_peak, auction.total_lt_off_peak,
+      auction.total_hts_peak, auction.total_hts_off_peak,
+      auction.total_htl_peak, auction.total_htl_off_peak
+    )
+    average_price = set_average_price(total_award_sum, total_volume)
+
+    history = user_auction_histories.first_or_create
+    history.update(
+      lt_peak: calculate_dto.lt_peak,
+      lt_off_peak: calculate_dto.lt_off_peak,
+      hts_peak: calculate_dto.hts_peak,
+      hts_off_peak: calculate_dto.hts_off_peak,
+      htl_peak: calculate_dto.htl_peak,
+      htl_off_peak: calculate_dto.htl_off_peak,
+      bid_time: auction.actual_begin_time,
+      actual_bid_time: auction.actual_begin_time,
+      average_price: average_price,
+      total_award_sum: total_award_sum,
+      is_bidder: true
+    )
+
+    find_sort_update_auction_histories(calculate_dto.auction_id)
   end
 
   def self.sort_update_auction_histories(histories)
-    # code here
     histories = histories.order(average_price: :asc, actual_bid_time: :asc)
+
     count = 0
     tmp_average_price = nil
     histories.each_with_index do |history, index|
@@ -66,14 +73,16 @@ class AuctionHistory < ApplicationRecord
       else
         count = 0
       end
-      # puts history, index
       history.update(ranking: index + 1 - count)
       tmp_average_price = history.average_price
     end
   end
 
-  def self.set_total_award_sum(c1, c2, c3, c4, c5, c6, p1, p2, p3, p4, p5, p6)
-    BigDecimal.new(c1) * BigDecimal.new(p1) + BigDecimal.new(c2) * BigDecimal.new(p2) + BigDecimal.new(c3) * BigDecimal.new(p3) + BigDecimal.new(c4) * BigDecimal.new(p4) + BigDecimal.new(c5) * BigDecimal.new(p5) + BigDecimal.new(c6) * BigDecimal.new(p6)
+  # Original args: c1, c2, c3, c4, c5, c6, p1, p2, p3, p4, p5, p6
+  def self.set_total_award_sum(quantities, prices)
+    quantities.zip(prices).inject(BigDecimal.new('0')) do |sum, n|
+      sum + (BigDecimal.new(n[0]) * BigDecimal.new(n[1]))
+    end
   end
 
   def self.set_average_price(total_award_sum, total_volume)
@@ -127,9 +136,5 @@ class AuctionHistory < ApplicationRecord
     # histories = AuctionHistory.find(ids)
     histories = AuctionHistory.select('auction_histories.* , users.company_name').joins(:user).find(ids)
     histories
-  end
-
-  def self.find_bidder_retailer_histories(auction_id)
-    AuctionHistory.where('auction_id = ? and is_bidder = ?', auction_id, true)
   end
 end
--- spec/models/auction_history_spec.rb
+++ spec/models/auction_history_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe AuctionHistory, type: :model do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:auction) }
   end
+
+  describe '.find_bidder_histories' do
+    let!(:auction) { create(:auction) }
+    let!(:user) { create(:user) }
+    let!(:bidder_history) { create(:auction_history, auction: auction, user: user, is_bidder: true) }
+    let!(:non_bidder_history) { create(:auction_history, auction: auction, user: user, is_bidder: false) }
+
+    it 'returns bidders' do
+      result = AuctionHistory.find_bidder_histories(auction.id)
+      expect(result.count).to eq 1
+      expect(result.first.id).to eq bidder_history.id
+    end
+  end
+
+  describe '.set_total_award_sum' do
+    it 'returns total award sum' do
+      result = AuctionHistory.set_total_award_sum(
+        ['2', '2', '2', '2', '2', '2'],
+        ['3', '3', '3', '3', '3', '3']
+      )
+
+      expect(result).to eq 36
+    end
+  end
 end
