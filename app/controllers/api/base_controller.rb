5777e07dbfb2ddda76599f0cc6e66d151ca36946
liuziyuan <liuziyuan.room@gmail.com>
2017-11-23 15:15:52 +0800

    add heartbeat for post

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,3 +1,7 @@
 class Api::BaseController < ApplicationController
   before_action :authenticate_user!
+
+  def heartbeat
+    render json: nil, status: 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,12 @@ Rails.application.routes.draw do
   end
 
   namespace :api do
+    resources :base, only: %i[hearbeat] do
+      collection do
+        post 'heartbeat'
+      end
+    end
+
     resources :arrangements, only: %i[index show obtain update] do
       collection do
         get 'obtain'
