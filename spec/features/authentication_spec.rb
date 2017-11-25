d195bce2aa28415afabf366eec9f4d94c7ed75b3
Michael Cheng Chi Mun <MICHAELCCM@SINGAPOREPOWER.COM.SG>
2017-11-25 18:05:29 +0800

    Featrue test for creating and editing auction

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,13 +1,13 @@
 require 'feature_helper'
 
-RSpec.feature 'sign in' do
-  background do
+RSpec.describe 'sign in', type: :feature do
+  before do
     create(:user, :with_admin, email: 'admin@example.com', password: 'password')
     create(:user, :with_retailer, email: 'retailer@example.com', password: 'password')
   end
 
-  scenario 'user can sign in', js: true do
-    in_browser(:admin) do
+  describe 'admin user' do
+    it 'can sign in' do
       visit new_user_session_path
 
       within('form.new_user') do
@@ -20,8 +20,10 @@ RSpec.feature 'sign in' do
       expect(page).to have_content 'Manage Published Upcoming Reverse Auction'
       expect(page).to have_content 'View Past Reverse Auction'
     end
+  end
 
-    in_browser(:retailer) do
+  describe 'retailer user' do
+    it 'can sign in' do
       visit new_user_session_path
 
       within('form.new_user') do
---
+++ spec/features/create_edit_auction_spec.rb
@@ -0,0 +1,83 @@
+require 'feature_helper'
+
+RSpec.describe 'create / edit auction', type: :feature, js: true do
+  let(:admin_user) { create(:user, :with_admin) }
+
+  before do
+    login_as(admin_user)
+  end
+
+  xdescribe 'creates auction' do
+    let!(:auction) { create(:auction) }
+
+    it 'creates a new auction' do
+      visit new_admin_auction_path
+
+      fill_in 'name', with: 'SP Reverse Auction 0001'
+      fill_in 'start_datetime', with: '23-11-2017 21:03'
+      fill_in 'contract_period_start_date', with: '01-11-2017'
+      fill_in 'contract_period_end_date', with: '31-11-2017'
+      fill_in 'duration', with: '5'
+      fill_in 'reserve_price', with: '0.0841'
+      click_button 'Save'
+
+      expect(page).to have_content 'Successful'
+    end
+  end
+
+  describe 'edit auction' do
+    let!(:auction) do
+      create(:auction, :for_next_month, :upcoming)
+    end
+
+    it 'updates an existing auction' do
+      visit new_admin_auction_path
+
+      fill_in 'name', with: 'SP Reverse Auction 0001'
+      fill_in 'duration', with: '10'
+      fill_in 'reserve_price', with: '0.0821'
+      click_button 'Save'
+
+      expect(page).to have_content 'successfully saved'
+
+      auction.reload
+      expect(auction.name).to eq 'SP Reverse Auction 0001'
+      expect(auction.duration).to eq 10
+      expect(auction.reserve_price).to eq BigDecimal.new('0.0821')
+    end
+  end
+
+  describe 'publish auction' do
+    let!(:auction) do
+      create(:auction, :for_next_month, :upcoming)
+    end
+
+    it 'publishes an existing auction' do
+      visit new_admin_auction_path
+
+      click_button 'Publish'
+
+      expect(page).to have_content 'successfully published'
+
+      auction.reload
+      expect(auction.publish_status).to eq '1'
+    end
+  end
+
+  describe 'published auction' do
+    let!(:auction) do
+      create(:auction, :for_next_month, :upcoming, :published)
+    end
+
+    it 'not editable' do
+      visit new_admin_auction_path
+
+      expect(page).to have_field('name', disabled: true)
+      expect(page).to have_field('start_datetime', disabled: true)
+      expect(page).to have_field('contract_period_start_date', disabled: true)
+      expect(page).to have_field('contract_period_end_date', disabled: true)
+      expect(page).to have_field('duration', disabled: true)
+      expect(page).to have_field('reserve_price', disabled: true)
+    end
+  end
+end
