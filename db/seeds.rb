578084cc34751e64c38cb05d9d31b7b0421778dd
liuziyuan <liuziyuan.room@gmail.com>
2017-11-29 15:26:46 +0800

    remove retailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,11 +31,9 @@ retailers = [
   { name: 'Energy Supply Solutions', email: 'yap@energysupplysolutions.com.sg', company_name: 'Energy Supply Solutions', password: '5Qq2Qy8E'  },
   { name: 'Hyflux Energy', email: 'huixin_tang@hyflux.com', company_name: 'Hyflux Energy', password: '2Cy7Fw5J'  },
   { name: 'I Switch', email: 'Jovan.tang@iswitch.com.sg', company_name: 'I Switch', password: '9Fr6Yt4G'  },
-  { name: 'Keppel Electric', email: 'peiling.liew@kepinfra.com', company_name: 'Keppel Electric', password: '6Yz3Pd7Z'  },
   { name: 'PacificLight Energy', email: 'Eugene.he@pacificlight.com.sg', company_name: 'PacificLight Energy', password: '7Tr5Au2Q'  },
   { name: 'Red Dot Power', email: 'hafizah.ahmad@reddotpower.com.sg', company_name: 'Red Dot Power', password: '6Pc9Qk7S'  },
   { name: 'SembCorp Power', email: 'jenny.lye@sembcorp.com', company_name: 'SembCorp Power', password: '2Uk6Cr7G'  },
-  { name: 'Seraya Energy', email: 'lamkl@pseraya.com.sg', company_name: 'Seraya Energy', password: '6Fb5Fq3M'  },
   { name: 'Sunseap Energy', email: 'Ryan.Ang@sunseap.com', company_name: 'Sunseap Energy', password: '5Ve6Ay9J'  },
   { name: 'Tuas Power Supply', email: 'charlainechan@tuaspower.com.sg', company_name: 'Tuas Power Supply', password: '8Qk3Mk5L'  },
   { name: 'Union Power', email: 'Hermann@UnionPower.com.sg', company_name: 'Union Power', password: '6Yp9Uq3Y' }
