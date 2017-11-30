9054fda9cb13182d075b81c84cf8a7520115f933
Michael Cheng and Reuben Tan <pair+michaelccm+reubentwk@spgroup.com.sg>
2017-11-30 17:21:20 +0800

    Fix broken test due to change in display text

--- spec/features/auction_bidding_spec.rb
+++ spec/features/auction_bidding_spec.rb
@@ -94,7 +94,7 @@ RSpec.describe 'auction bidding', type: :feature, js: true do
       in_browser(:admin) do
         expect(all('table.retailer_fill tbody')[0]).to have_selector('tr', wait: 5) # Wait for the row to be populated
         expect(all('table.retailer_fill tbody tr')[0]).to have_selector('td', text: retailer_user.company_name)
-        expect(all('table.retailer_fill tbody tr')[0]).to have_selector('td', text: '0.1222')
+        expect(all('table.retailer_fill tbody tr')[0]).to have_selector('td', text: '$ 0.1222/kWh')
       end
 
       Timecop.travel(DateTime.now.advance(minutes: 30).to_time)
