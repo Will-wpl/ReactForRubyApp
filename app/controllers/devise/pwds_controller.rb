95fff5de58abb9d6e4080e8587888ea6dad19e2d
liuziyuan <liuziyuan.room@gmail.com>
2017-12-22 11:07:57 +0800

    completed pwd edit

--- app/controllers/devise/pwds_controller.rb
+++ app/controllers/devise/pwds_controller.rb
@@ -1,23 +1,23 @@
 class Devise::PwdsController < DeviseController
-  prepend_before_action :authenticate_user!
+  prepend_before_action :authenticate_scope!, only: %i[edit update]
   prepend_before_action :set_minimum_password_length, only: %i[edit]
+
   before_action :set_user, only: %i[edit update]
 
   def edit; end
 
   def update
-    update_user_params = model_params
-
-    if update_user_params[:password].blank?
-      update_user_params.delete('password')
-      update_user_params.delete('password_confirmation')
-    end
-
-    if @user.update(update_user_params)
-      redirect_to edit_user_registration_path, notice: "#{User.model_name.human} was successfully updated."
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      bypass_sign_in resource, scope: resource_name
+      redirect_to users_pwds_edit_path, notice: "#{User.model_name.human} was successfully updated."
     else
-      render :edit_account
+      clean_up_passwords resource
+      set_minimum_password_length
+      render :edit
     end
+
     # self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     # prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
     #
@@ -40,6 +40,18 @@ class Devise::PwdsController < DeviseController
 
   private
 
+  # By default we want to require a password checks on update.
+  # You can overwrite this method in your own RegistrationsController.
+  def update_resource(resource, params)
+    resource.update_with_password(params)
+  end
+
+  # Authenticates the current scope and gets the current resource from the session.
+  def authenticate_scope!
+    send(:"authenticate_#{resource_name}!", force: true)
+    self.resource = send(:"current_#{resource_name}")
+  end
+
   def set_user
     @user = User.find(current_user.id)
   end
@@ -50,5 +62,9 @@ class Devise::PwdsController < DeviseController
                                  :company_unique_entity_number, :company_license_number, :account_fin,
                                  :account_mobile_number, :account_office_number, :account_home_number,
                                  :account_housing_type, :account_home_address)
+  end
+
+  def account_update_params
+    devise_parameter_sanitizer.sanitize(:account_update)
   end
 end
