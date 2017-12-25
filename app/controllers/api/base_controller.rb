25f224294fc35bae75013817f26a28ea44816554
liuziyuan <liuziyuan.room@gmail.com>
2017-12-25 16:39:47 +0800

    add retailer list pager and unit test

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -7,6 +7,42 @@ class Api::BaseController < ApplicationController
 
   protected
 
+  def set_search_params(params)
+    where_params = reject_params(params, %w(page_size page_index))
+    where_conditions = []
+    where_attributes = []
+    where_params.each do |key,value|
+      unless value[0] == ''
+        if value[1] == 'like'
+          where_conditions.push("#{key} like ?")
+          where_attributes.push("%#{value[0]}%")
+        end
+        if value[1] == '='
+          where_conditions.push("#{key} = ?")
+          where_attributes.push(value[0])
+        end
+      end
+    end
+    where = []
+    where.push(where_conditions.join(' and '))
+    where + where_attributes
+  end
+
+  def reject_params(params, reject_list)
+    rejected_params = params.reject do |key|
+      reject_list.include?(key)
+    end
+    rejected_params
+  end
+
+  def select_params(params, select_list)
+    selected_params = params.select do |key|
+      select_list.include?(key)
+    end
+    selected_params
+  end
+
+
   def admin_required
     head :unauthorized unless current_user&.has_role?(:admin)
   end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,23 @@
 class Api::UsersController < Api::BaseController
   # user.approval_status['0', '1', '2'] '0':rejected '1':approved '2':pending
   def retailers
+    total = nil
+    if params.key?(:page_size) && params.key?(:page_index)
+      search_parmas = reject_params(params, %w[controller action])
+      search_where_array = set_search_params(search_parmas)
+      users = User.retailers.where(search_where_array)
+                  .page(params[:page_index]).per(params[:page_size])
+      total = users.total_count
+    else
+      users = User.retailers
+      total = users.count
+    end
     headers = []
     actions = []
-    headers.push(name: 'Company Name', index: 5)
-    headers.push(name: 'License Number', index: 10)
-    headers.push(name: 'Status', index: 6)
-    data = User.retailers.order(created_at: :desc).each do |user|
+    headers.push(name: 'Company Name', field_name: 'company_name')
+    headers.push(name: 'License Number', field_name: 'company_license_number')
+    headers.push(name: 'Status', field_name: 'approval_status')
+    data = users.order(created_at: :desc).each do |user|
       user.approval_status = if user.approval_status == '0'
                                'Rejected'
                              elsif user.approval_status == '2'
@@ -15,9 +26,8 @@ class Api::UsersController < Api::BaseController
                                'Approved'
                              end
     end
-    total = data.count
-    bodies = {data: data, total: total}
-    actions.push(url: 'users/:id/manage', name: 'Manage', icon: 'lm--icon-search')
+    bodies = { data: data, total: total }
+    actions.push(url: '/admin/users/:id/manage', name: 'Manage', icon: 'lm--icon-search')
     render json: { headers: headers, bodies: bodies, actions: actions }, status: 200
   end
 
@@ -35,7 +45,7 @@ class Api::UsersController < Api::BaseController
     end
     total = data.count
     bodies = { data: data, total: total }
-    actions.push(url: 'users/:id/manage', name: 'View', icon: 'lm--icon-search')
+    actions.push(url: '/admin/users/:id/manage', name: 'View', icon: 'lm--icon-search')
     render json: { headers: headers, bodies: bodies, actions: actions }, status: 200
   end
 end
