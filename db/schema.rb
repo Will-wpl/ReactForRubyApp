adcb4cb5730efc3823e90c29fe6e3b81a8f1ea8e
liuziyuan <liuziyuan.room@gmail.com>
2017-12-26 10:35:01 +0800

    add comment field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219091938) do
+ActiveRecord::Schema.define(version: 20171226023226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -181,6 +181,7 @@ ActiveRecord::Schema.define(version: 20171219091938) do
     t.string "account_home_number"
     t.string "account_housing_type"
     t.string "account_home_address"
+    t.text "comment"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
