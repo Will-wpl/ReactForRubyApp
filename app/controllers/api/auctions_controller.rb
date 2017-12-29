2cc45e107ff2066ca0c22afd527cef0517a38de2
liuziyuan <liuziyuan.room@gmail.com>
2017-12-29 15:35:48 +0800

    add default publish status

--- app/controllers/api/auctions_controller.rb
+++ app/controllers/api/auctions_controller.rb
@@ -15,6 +15,7 @@ class Api::AuctionsController < Api::BaseController
   # PATCH update auction by ajax
   def update
     if params[:id] == '0' # create
+      params[:auction]['publish_status'] = '0'
       if @auction.save
         AuctionEvent.set_events(current_user.id, @auction.id, request[:action], @auction.to_json)
         render json: @auction, status: 201
