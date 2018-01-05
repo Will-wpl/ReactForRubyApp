278942ccdd0bfd1bd28034127273ab8de01a4a81
liuziyuan <liuziyuan.room@gmail.com>
2018-01-05 15:05:58 +0800

    add field to arrangement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227035802) do
+ActiveRecord::Schema.define(version: 20180105065627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171227035802) do
     t.string "accept_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "action_status"
     t.index ["auction_id"], name: "index_arrangements_on_auction_id"
     t.index ["user_id"], name: "index_arrangements_on_user_id"
   end
