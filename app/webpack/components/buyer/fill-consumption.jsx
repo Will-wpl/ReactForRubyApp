5358f7698ad7fae7f60347119fe3d4933f2e29a9
Brian <yangqingxin83@163.com>
2018-01-08 16:52:52 +0800

    Correct labels

--- app/webpack/components/buyer/fill-consumption.jsx
+++ app/webpack/components/buyer/fill-consumption.jsx
@@ -13,7 +13,7 @@ export class DoFillConsumption extends Component {
                              <h4 className="site_list_title"><span>{index+1}.My Site Information</span>{index != 0 ? <a onClick={this.props.remove.bind(this,index)}>Delete</a> : ''}</h4>
                              <table className="retailer_fill u-mb3" cellPadding="0" cellSpacing="0">
                                  <tbody>
-                                     <tr><td>Accound Number</td><td><input type="text" defaultValue={`${item.number}${index+1}`} id={"number"+(index+1)} required aria-required="true" name={"number"+(index+1)}/></td></tr>
+                                     <tr><td>Account Number</td><td><input type="text" defaultValue={`${item.number}${index+1}`} id={"number"+(index+1)} required aria-required="true" name={"number"+(index+1)}/></td></tr>
                                      <tr><td>Intake Level</td>
                                         <td>
                                             <select id={"level"+(index+1)} name={"level"+(index+1)}>
@@ -24,8 +24,8 @@ export class DoFillConsumption extends Component {
                                             }
                                             </select>
                                         </td></tr>
-                                     <tr><td>Peak Volume (kWh/mouth)</td><td><input type="text" required aria-required="true" name={"peak"+(index+1)} id={"peak"+(index+1)} defaultValue={item.peak}/></td></tr>
-                                     <tr><td>Off-peak Volume (kWh/mouth)</td><td><input type="text" required aria-required="true" name={"off_peak"+(index+1)} defaultValue={item.off_peak} id={"off_peak"+(index+1)}/></td></tr>
+                                     <tr><td>Peak Volume (kWh/month)</td><td><input type="text" required aria-required="true" name={"peak"+(index+1)} id={"peak"+(index+1)} defaultValue={item.peak}/></td></tr>
+                                     <tr><td>Off-peak Volume (kWh/month)</td><td><input type="text" required aria-required="true" name={"off_peak"+(index+1)} defaultValue={item.off_peak} id={"off_peak"+(index+1)}/></td></tr>
                                  </tbody>
                              </table>
                           </div>
