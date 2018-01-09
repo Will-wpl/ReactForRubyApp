51b3b288bcf42f1049509734c9a9c0232c5d2a02
liuziyuan <liuziyuan.room@gmail.com>
2018-01-09 19:34:13 +0800

    add user_id field to attachment

--- app/models/auction_attachment.rb
+++ app/models/auction_attachment.rb
@@ -6,13 +6,14 @@ class AuctionAttachment < ApplicationRecord
 
   # Associations
   belongs_to :auction
+  belongs_to :user
 
   # accepts_nested_attributes
 
   # Validations
 
   # Scopes
-
+  scope :belong_auction, ->(auction_id) { where('auction_id = ? and user_id is null', auction_id) }
   # Callbacks
 
   # Delegates
