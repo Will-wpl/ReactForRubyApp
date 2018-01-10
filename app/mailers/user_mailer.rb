018616c6b3f621143c451df2ff838ab20efdf11c
Brian <yangqingxin83@163.com>
2018-01-10 17:22:59 +0800

    Add invited email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -21,15 +21,35 @@ class UserMailer < ApplicationMailer
     send_email(user.email, email_body, email_subject)
   end
 
-  private
-    def send_email(email, body, subject)
-      mail(to: email,
-           body: body,
-           content_type: "text/html",
-           subject: subject)
-    end
+  def retailer_invited_email(user)
+    mail_template = get_template('5')
+    email_subject = mail_template.subject
+    email_body = mail_template.body.gsub(/#user.company_name/, user.company_name)
+    send_email(user.email, email_body, email_subject)
+  end
 
-    def get_template(type)
-      EmailTemplate.find_by_template_type(type)
+  def buyer_invited_email(user)
+    mail_template = get_template('4')
+    email_subject = mail_template.subject
+    email_body = mail_template.body
+    if user.consumer_type == '2'
+      email_body = email_body.gsub(/#buyer.name/, user.company_name)
+    elsif user.consumer_type == '3'
+      email_body = email_body.gsub(/#buyer.name/, user.name)
     end
+    send_email(user.email, email_body, email_subject)
+  end
+
+  private
+
+  def send_email(email, body, subject)
+    mail(to: email,
+         body: body,
+         content_type: "text/html",
+         subject: subject)
+  end
+
+  def get_template(type)
+    EmailTemplate.find_by_template_type(type)
+  end
 end
