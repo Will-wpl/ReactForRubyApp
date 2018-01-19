141b2415b880012fb023c10e8981a989101316c1
liuziyuan <liuziyuan.room@gmail.com>
2018-01-19 09:31:05 +0800

    add auction name to retailer obtain

--- app/controllers/api/retailer/auctions_controller.rb
+++ app/controllers/api/retailer/auctions_controller.rb
@@ -6,7 +6,7 @@ class Api::Retailer::AuctionsController < Api::AuctionsController
     else
       auction = Auction.find(params[:id])
       if Arrangement.auction_of_current_user(auction.id, current_user.id).exists?
-        render json: { id: auction.id, publish_status: auction.publish_status }, status: 200
+        render json: { id: auction.id, publish_status: auction.publish_status, name: auction.name }, status: 200
       else
         render json: { message: 'you can not get the auction information.' }, status: 400
       end
