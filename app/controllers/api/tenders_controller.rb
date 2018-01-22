0a1d0b8422b23297d866f376faf4b6aaa871cdde
liuziyuan <liuziyuan.room@gmail.com>
2018-01-22 10:39:02 +0800

    revert to right version

--- app/controllers/api/tenders_controller.rb
+++ app/controllers/api/tenders_controller.rb
@@ -31,11 +31,12 @@ class Api::TendersController < Api::BaseController
                       .where(file_type: 'tender_documents_upload').count
     chats = []
     TenderChat.where(arrangement_id: params[:id]).each do |chat|
-      last_retailer_response = TenderChatDetail.last_retailer_response(chat.id)
-      last_sp_response = TenderChatDetail.last_admin_response(chat.id)
+      last_retailer_response = TenderChatDetail.retailer_response(chat.id).last
+      last_sp_response = TenderChatDetail.admin_response(chat.id).last
       chats.push(item: chat.item, clause: chat.clause, sp_response_status: chat.sp_response_status,
-                 retailer_response: last_retailer_response.retailer_response, propose_deviation: last_retailer_response.propose_deviation,
-                 sp_response: last_sp_response.sp_response)
+                 retailer_response: last_retailer_response.nil? ? nil : last_retailer_response.retailer_response,
+                 propose_deviation: last_retailer_response.nil? ? nil : last_retailer_response.propose_deviation,
+                 sp_response: last_sp_response.nil? ? nil : last_sp_response.sp_response)
     end
 
     render json: { chats: chats, attachments_count: attachments_count }, status: 200
@@ -86,7 +87,7 @@ class Api::TendersController < Api::BaseController
         tender_chat = set_tender_chat(chat, params[:id])
         next unless tender_chat.save
         chat_info = set_withdraw_tender_chat(tender_chat, chat)
-        TenderChatDetail.chat_save(chat_info)
+        TenderChatDetail.chat_save(tender_chat, chat_info)
       end
 
       workflow = TenderWorkflow.new.execute(:node3, :withdraw_all_deviations, params[:id])
@@ -103,7 +104,7 @@ class Api::TendersController < Api::BaseController
         tender_chat = set_tender_chat(chat, params[:id])
         next unless tender_chat.save
         chat_info = set_submit_deviation_tender_chat(tender_chat, chat)
-        TenderChatDetail.chat_save(chat_info)
+        TenderChatDetail.chat_save(tender_chat, chat_info)
       end
 
       workflow = TenderWorkflow.new.execute(:node3, :submit_deviations, params[:id])
@@ -156,9 +157,9 @@ class Api::TendersController < Api::BaseController
 
   def node3_retailer_withdraw
     chat = params[:chat]
-    tender_chat = TenderChat.find(params[:chat_id])
+    tender_chat = TenderChat.find(chat['id'])
     chat_info = set_withdraw_tender_chat(tender_chat, chat)
-    chat = TenderChatDetail.chat_save(chat_info)
+    chat = TenderChatDetail.chat_save(tender_chat, chat_info)
     render json: chat, status: 200
   end
 
@@ -169,7 +170,7 @@ class Api::TendersController < Api::BaseController
         tender_chat = set_tender_chat(chat, params[:id])
         next unless tender_chat.save
         chat_info = set_save_tender_chat(tender_chat, chat)
-        TenderChatDetail.chat_save(chat_info)
+        TenderChatDetail.chat_save(tender_chat, chat_info)
       end
     end
 
@@ -187,33 +188,33 @@ class Api::TendersController < Api::BaseController
   end
 
   def set_tender_chat(chat, arrangement_id)
-    if chat.id == 0 || chat.id.nil?
+    if chat['id'] == 0 || chat['id'].nil?
       tender_chat = TenderChat.new
-      tender_chat.item = chat.item
-      tender_chat.clause = chat.clause
+      tender_chat.item = chat['item']
+      tender_chat.clause = chat['clause']
       tender_chat.arrangement_id = arrangement_id
     else
-      tender_chat = TenderChat.find(chat.id)
-      tender_chat.item = chat.item
-      tender_chat.clause = chat.clause
+      tender_chat = TenderChat.find(chat['id'])
+      tender_chat.item = chat['item']
+      tender_chat.clause = chat['clause']
     end
     tender_chat
   end
 
   def set_withdraw_tender_chat(tender_chat, chat)
     chat_info = ChatInfoDto.new(tender_chat)
-    chat_info.propose_deviation = chat.propose_deviation
+    chat_info.propose_deviation = chat['propose_deviation']
     chat_info.retailer_response = 'I have withdrawn this deviation.'
     chat_info.sp_response = nil
     chat_info.response_status = '1'
-    chat_info.sp_response_status = '0'
+    chat_info.sp_response_status = '1'
     chat_info
   end
 
   def set_save_tender_chat(tender_chat, chat)
     chat_info = ChatInfoDto.new(tender_chat)
-    chat_info.propose_deviation = chat.propose_deviation
-    chat_info.retailer_response = chat.retailer_response
+    chat_info.propose_deviation = chat['propose_deviation']
+    chat_info.retailer_response = chat['retailer_response']
     chat_info.sp_response = nil
     chat_info.response_status = '2'
     chat_info.sp_response_status = '2'
@@ -222,8 +223,8 @@ class Api::TendersController < Api::BaseController
 
   def set_submit_deviation_tender_chat(tender_chat, chat)
     chat_info = ChatInfoDto.new(tender_chat)
-    chat_info.propose_deviation = chat.propose_deviation
-    chat_info.retailer_response = chat.retailer_response
+    chat_info.propose_deviation = chat['propose_deviation']
+    chat_info.retailer_response = chat['retailer_response']
     chat_info.sp_response = nil
     chat_info.response_status = '3'
     chat_info.sp_response_status = '1'
