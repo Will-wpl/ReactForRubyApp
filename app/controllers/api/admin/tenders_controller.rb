3079530c331b3684343c16b37c985de6ac0f1390
liuziyuan <liuziyuan.room@gmail.com>
2018-01-23 11:06:31 +0800

    add to set arrangement

--- app/controllers/api/admin/tenders_controller.rb
+++ app/controllers/api/admin/tenders_controller.rb
@@ -1,4 +1,4 @@
 class Api::Admin::TendersController < Api::TendersController
   before_action :admin_required
-  before_action :set_arrangement, only: %i[node4_admin_accept node4_admin_reject]
+  before_action :set_arrangement, only: %i[node4_admin_accept node4_admin_reject node4_admin]
 end
