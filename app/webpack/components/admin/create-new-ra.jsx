5239d33b982a4c2ac45cad7b4fea0688cfccbba2
will <Will.wang@chinasofti.com>
2018-01-29 16:16:06 +0800

    workflow back readOnly

--- app/webpack/components/admin/create-new-ra.jsx
+++ app/webpack/components/admin/create-new-ra.jsx
@@ -261,8 +261,7 @@ export class CreateNewRA extends Component {
             if(this.props.left_name){
                 this.setState({
                     edit_btn:"lm--button lm--button--primary show",
-                    edit_change:"lm--button lm--button--primary hide",
-                    disabled:true
+                    edit_change:"lm--button lm--button--primary hide"
                 })
             }
         }
