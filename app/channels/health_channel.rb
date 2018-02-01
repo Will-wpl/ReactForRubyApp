b37ab09596f0042a36d69f7e0b7a8eb4127d1edb
liuziyuan <liuziyuan.room@gmail.com>
2018-02-01 10:24:55 +0800

    modify

--- app/channels/health_channel.rb
+++ app/channels/health_channel.rb
@@ -1,11 +1,9 @@
 class HealthChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "health_#{params[:room]}"
-  end
+  def subscribed; end
 
   def unsubscribed; end
 
   def heartbeat(data)
-    ActionCable.server.broadcast "health_#{params[:room]}", action: 'heartbeat', data: 'hello'
+    
   end
 end
