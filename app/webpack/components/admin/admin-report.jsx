1a045f45775ff11d5536d0010d7a0ea75496a78d
huangsonglu <jason.huang@chinasofti.com>
2018-02-01 17:13:22 +0800

    set visibility of report's confirm winner price

--- app/webpack/components/admin/admin-report.jsx
+++ app/webpack/components/admin/admin-report.jsx
@@ -136,7 +136,7 @@ export class AdminReport extends Component {
                         </div>
                     </div>
                     <div className="col-sm-12 col-md-5">
-                        <WinnerPrice showOrhide="show" winner={this.state.winner} />
+                        <WinnerPrice showOrhide="show" winner={this.state.winner} isLtVisible={visibility_lt} isHtsVisible={visibility_hts} isHtlVisible={visibility_htl} isEhtVisible={visibility_eht}/>
                         <RetailerRanking ranking={this.state.ranking}/>
                     </div>
                 </div>
