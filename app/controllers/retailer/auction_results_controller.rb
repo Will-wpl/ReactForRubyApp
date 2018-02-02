fff55132ddbe178e041669f437fcba5196db65d5
liuziyuan <liuziyuan.room@gmail.com>
2018-02-02 13:56:21 +0800

    joined auction result

--- app/controllers/retailer/auction_results_controller.rb
+++ app/controllers/retailer/auction_results_controller.rb
@@ -1,6 +1,7 @@
 class Retailer::AuctionResultsController < Retailer::BaseController
   before_action :retailer_approved_required
   def index
-    @auction_results = AuctionResult.select('auction_results.* , auctions.name , auctions.start_datetime').joins(:auction).order(created_at: :desc).page(params[:page])
+    @auction_results = AuctionResult.select('auction_results.* , auctions.name , auctions.start_datetime')
+                                    .joins(auction: :arrangements).where('arrangements.user_id = ?', current_user.id).order(created_at: :desc).page(params[:page])
   end
 end
