1000ad4f49212b0f5cd194b6be9b50a271cd51c0
liuziyuan <liuziyuan.room@gmail.com>
2018-02-12 13:53:11 +0800

    base buyer post ra index function

--- app/models/auction_result.rb
+++ app/models/auction_result.rb
@@ -13,7 +13,7 @@ class AuctionResult < ApplicationRecord
 
   # Scopes
   scope :find_by_arrangement, ->(user) { joins(auction: :arrangements).where(arrangements: { user_id: user }) }
-
+  scope :find_by_consumptions, ->(user) { joins(auction: :consumptions).where(consumptions: { user_id: user }) }
   # Callbacks
 
   # Delegates
