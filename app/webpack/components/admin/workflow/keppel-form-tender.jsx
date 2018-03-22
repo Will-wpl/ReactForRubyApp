a8258383c27399c85dd28be45f977a38d731d8e8
will <Will.wang@chinasofti.com>
2018-03-22 15:22:51 +0800

    add decodeURI

--- app/webpack/components/admin/workflow/keppel-form-tender.jsx
+++ app/webpack/components/admin/workflow/keppel-form-tender.jsx
@@ -91,9 +91,9 @@ export class Keppelformtender extends React.Component{
                                     return <tr key={index}>
                                                 <td>{item.item}</td>
                                                 <td >{item.clause}</td>
-                                                <td ><textarea className="show_text" defaultValue={item.propose_deviation.replace(/＜br＞/g,"\n")} disabled/></td>
-                                                <td ><textarea className="show_text" defaultValue={item.retailer_response.replace(/＜br＞/g,"\n")} disabled/></td>
-                                                <td ><textarea className="show_text" defaultValue={item.sp_response.replace(/＜br＞/g,"\n")} disabled/></td>
+                                                <td ><textarea className="show_text" defaultValue={decodeURI(item.propose_deviation)} disabled/></td>
+                                                <td ><textarea className="show_text" defaultValue={decodeURI(item.retailer_response)} disabled/></td>
+                                                <td ><textarea className="show_text" defaultValue={decodeURI(item.sp_response)} disabled/></td>
                                                 <td><button onClick={this.showhistory.bind(this,item.id)}>History</button></td>
                                             </tr>
                                 })}
