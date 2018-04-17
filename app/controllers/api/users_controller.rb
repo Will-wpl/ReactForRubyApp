e2ca5d549080792d055efc16970e7cfdeaad367e
liuziyuan <liuziyuan.room@gmail.com>
2018-04-17 15:00:42 +0800

    add reatilers and buyers sort function at User controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < Api::BaseController
   # user.approval_status['0', '1', '2'] '0':rejected '1':approved '2':pending
   def retailers
     if params.key?(:page_size) && params.key?(:page_index)
-      search_params = reject_params(params, %w[controller action])
+      search_params = reject_params(params, %w[controller action sort_by])
       search_where_array = set_search_params(search_params)
       users = User.retailers.where(search_where_array)
                   .page(params[:page_index]).per(params[:page_size])
@@ -17,7 +17,13 @@ class Api::UsersController < Api::BaseController
       { name: 'Status', field_name: 'approval_status' }
     ]
     actions = [{ url: '/admin/users/:id/manage', name: 'Manage', icon: 'manage' }]
-    data = users.order(approval_status: :desc, company_name: :asc).each do |user|
+    users = if params.key?(:sort_by)
+              order_by_string = get_order_by_string(params[:sort_by])
+              users.order(order_by_string)
+            else
+              users.order(approval_status: :desc, company_name: :asc)
+            end
+    data = users.each do |user|
       user.approval_status = if user.approval_status == '0'
                                'Rejected'
                              elsif user.approval_status == '1'
@@ -35,7 +41,7 @@ class Api::UsersController < Api::BaseController
   # user.user_detail.consumer_type['0', '1'] '0':company '1':individual
   def buyers
     if params.key?(:page_size) && params.key?(:page_index)
-      search_params = reject_params(params, %w[controller action])
+      search_params = reject_params(params, %w[controller action sort_by])
       if !params[:name].nil? && params[:consumer_type][0] == '2'
         company_name = { company_name: params[:name] }
         search_params = reject_params(search_params, %w[name])
@@ -51,7 +57,7 @@ class Api::UsersController < Api::BaseController
     end
     headers = [
       { name: 'Company Name', field_name: 'company_name' },
-      { name: 'Name', field_name: 'name' },
+      { name: 'Name', field_name: 'name', table_name: 'users' },
       { name: 'Email', field_name: 'email' },
       { name: 'Consumer Type', field_name: 'consumer_type' }
     ]
@@ -61,13 +67,33 @@ class Api::UsersController < Api::BaseController
       headers.delete_if { |header| header[:field_name] == 'company_name' } if params[:consumer_type][0] == '3'
     end
     data = if params[:consumer_type].nil?
-             users
+             if params.key?(:sort_by)
+               order_by_string = get_order_by_string(params[:sort_by])
+               users.order(order_by_string)
+             else
+               users
+             end
            elsif params[:consumer_type][0] == '2'
-             users.order(company_name: :asc)
+             if params.key?(:sort_by)
+               order_by_string = get_order_by_string(params[:sort_by])
+               users.order(order_by_string)
+             else
+               users.order(company_name: :asc)
+             end
            elsif params[:consumer_type][0] == '3'
-             users.order(name: :asc)
+             if params.key?(:sort_by)
+               order_by_string = get_order_by_string(params[:sort_by])
+               users.order(order_by_string)
+             else
+               users.order(name: :asc)
+             end
            else
-             users
+             if params.key?(:sort_by)
+               order_by_string = get_order_by_string(params[:sort_by])
+               users.order(order_by_string)
+             else
+               users
+             end
            end
     data = data.each do |user|
       user.consumer_type = user.consumer_type == '2' ? 'Company' : 'Individual'
