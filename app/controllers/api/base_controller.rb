b2f922bb7473ba876014086ac8286996d9df7271
liuziyuan <liuziyuan.room@gmail.com>
2018-04-23 16:08:35 +0800

    try to fix order SQL issue

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -8,33 +8,31 @@ class Api::BaseController < ApplicationController
   protected
 
   def set_search_params(params)
-    where_params = reject_params(params, %w(page_size page_index))
+    where_params = reject_params(params, %w[page_size page_index])
     where_conditions = []
     where_attributes = []
-    where_params.each do |key,value|
-      unless value[0] == ''
-        table_name = value[2].nil? ? nil : value[2] + '.'
-        if value[1] == 'like'
-          where_conditions.push("#{table_name}#{key} ilike ?")
-          where_attributes.push("%#{value[0]}%")
-        end
-        if value[1] == '='
-          where_conditions.push("#{table_name}#{key} = ?")
-          where_attributes.push(value[0])
-        end
-        if value[1] == 'date_between'
-          date_time = Time.parse(value[0])
-          where_conditions.push("#{table_name}#{key} between ? and ?")
-          where_attributes.push(date_time.beginning_of_day)
-          where_attributes.push(date_time.at_end_of_day)
-        end
+    where_params.each do |key, value|
+      next if value[0] == ''
+      table_name = value[2].nil? ? nil : value[2] + '.'
+      if value[1] == 'like'
+        where_conditions.push("#{table_name}#{key} ilike ?")
+        where_attributes.push("%#{value[0]}%")
       end
+      if value[1] == '='
+        where_conditions.push("#{table_name}#{key} = ?")
+        where_attributes.push(value[0])
+      end
+      next unless value[1] == 'date_between'
+      date_time = Time.parse(value[0])
+      where_conditions.push("#{table_name}#{key} between ? and ?")
+      where_attributes.push(date_time.beginning_of_day)
+      where_attributes.push(date_time.at_end_of_day)
     end
     where = []
     where.push(where_conditions.join(' and '))
     condition = where + where_attributes
-    if condition == [""]
-      "1 = 1"
+    if condition == ['']
+      '1 = 1'
     else
       condition
     end
@@ -45,10 +43,15 @@ class Api::BaseController < ApplicationController
     order = sort_by[1]
     table_name = sort_by[2]
     order_by_string = if table_name == ''
-                        field_name + ' ' + order
+                        field_name + ' '
                       else
-                        table_name + '.' + field_name + ' ' + order
+                        table_name + '.' + field_name + ' '
                       end
+    order_by_string += if order == 'asc'
+                         'ASC'
+                       else
+                         'DESC'
+                       end
     order_by_string
   end
 
@@ -75,7 +78,6 @@ class Api::BaseController < ApplicationController
     end
     ids
   end
-
 
   def admin_required
     head :unauthorized unless current_user&.has_role?(:admin)
