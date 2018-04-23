4882b7d1ac56bce0303345b796f9a309bbeefb74
Brian <yangqingxin83@163.com>
2018-04-23 10:16:39 +0800

    delete Buyer and Common files useless annotation

--- app/webpack/components/common/chart/list-checkbox-item.js
+++ app/webpack/components/common/chart/list-checkbox-item.js
@@ -18,7 +18,6 @@ export default class CheckboxListItem extends Component {
     render() {
         return (
             <li className="checkitem" onClick={this.onItemClick.bind(this, this.props.id, !this.state.status, this.props.color)} style={{ color:this.props.color,cursor: 'pointer' }}>
-                {/* <input type="checkbox" onChange={this.onItemChecked.bind(this, this.props.id)} /> */}
                 <img src={this.state.status ? selected : unselected} alt="selectstatus" />
                 <span>{this.props.display}</span>
             </li>
