e6b20079085236719fe5b314d519bd4a2657bba9
will <Will.wang@chinasofti.com>
2018-04-24 17:31:59 +0800

    code review

--- app/webpack/components/retailer/workflow/sign-confidentiality-undertaking.jsx
+++ app/webpack/components/retailer/workflow/sign-confidentiality-undertaking.jsx
@@ -18,44 +18,34 @@ export class Signconfidentialityundertaking extends React.Component{
             })
         })
     }
+    doconfirm(type){
+        this.refs.Modal.showModal("comfirm");
+        this.setState({
+            text:`Are you sure you want to ${type} the Confidentiality Undertaking ?`
+        });
+    }
     showConfirm(type){
         this.setState({buttonType:type});
-        if(type == "Reject"){
-            this.refs.Modal.showModal("comfirm");
-            this.setState({
-                text:"Are you sure you want to reject the Confidentiality Undertaking ?"
-            });
-        }else{
-            this.refs.Modal.showModal("comfirm");
-            this.setState({
-                text:"Are you sure you want to accept the Confidentiality Undertaking ?"
-            });
-        }
+        this.doconfirm(type);
+    }
+    popUp(text){
+        this.refs.Modal.showModal();
+        this.setState({text:text});
     }
     do_reject(){
         retailerReject(this.props.current.current.arrangement_id).then(res=>{
-            this.refs.Modal.showModal();
-            this.setState({
-                text:"Thank you for the confirmation. You have rejected the Confidentiality Undertaking."
-            });
+            this.popUp("Thank you for the confirmation. You have rejected the Confidentiality Undertaking.");
             setTimeout(()=>{
                 window.location.href="/retailer/auctions";
             },3000)
-        },error=>{
-
         })
     }
     do_accept(){
         retailerAccept(this.props.current.current.arrangement_id).then(res=>{
-            this.refs.Modal.showModal();
-            this.setState({
-                text:"You have accepted the Confidentiality Undertaking."
-            });
+            this.popUp("You have accepted the Confidentiality Undertaking.");
             setTimeout(()=>{
                 this.props.page();
             },3000)
-        },error=>{
-
         })
     }
     render(){
