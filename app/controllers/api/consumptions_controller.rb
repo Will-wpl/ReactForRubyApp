8684a040737265f20c60ff19ab1134fb7e0c9a74
liuziyuan <liuziyuan.room@gmail.com>
2018-04-24 11:41:08 +0800

    fix code climate issue

--- app/controllers/api/consumptions_controller.rb
+++ app/controllers/api/consumptions_controller.rb
@@ -2,10 +2,11 @@ class Api::ConsumptionsController < Api::BaseController
   before_action :set_consumption, only: %i[show update_status destroy acknowledge]
 
   def index
+    consumptions = Consumption.find_by_user_consumer_type(params[:consumer_type]).find_by_auction_id(params[:id]).is_participation
     if params[:consumer_type] == '2'
-      consumptions = Consumption.find_by_user_consumer_type(params[:consumer_type]).find_by_auction_id(params[:id]).is_participation.order('users.company_name asc')
+      consumptions = consumptions.order('users.company_name asc')
     else
-      consumptions = Consumption.find_by_user_consumer_type(params[:consumer_type]).find_by_auction_id(params[:id]).is_participation.order('users.name asc')
+      consumptions = consumptions.order('users.name asc')
     end
     data = []
     total_info = { consumption_count: 0, account_count: 0, lt_peak: 0, lt_off_peak: 0,
@@ -19,25 +20,25 @@ class Api::ConsumptionsController < Api::BaseController
                 company_name: consumption.user.company_name,
                 name: consumption.user.name,
                 count: count,
-                lt_peak: consumption.lt_peak.nil? ? 0 : consumption.lt_peak,
-                lt_off_peak: consumption.lt_off_peak.nil? ? 0 : consumption.lt_off_peak,
-                hts_peak: consumption.hts_peak.nil? ? 0 : consumption.hts_peak,
-                hts_off_peak: consumption.hts_off_peak.nil? ? 0 : consumption.hts_off_peak,
-                htl_peak: consumption.htl_peak.nil? ? 0 : consumption.htl_peak,
-                htl_off_peak: consumption.htl_off_peak.nil? ? 0 : consumption.htl_off_peak,
-                eht_peak: consumption.eht_peak.nil? ? 0 : consumption.eht_peak,
-                eht_off_peak: consumption.eht_off_peak.nil? ? 0 : consumption.eht_off_peak,
+                lt_peak: Consumption.get_lt_peak(consumption.lt_peak),
+                lt_off_peak: Consumption.get_lt_off_peak(consumption.lt_off_peak),
+                hts_peak: Consumption.get_hts_peak(consumption.hts_peak),
+                hts_off_peak: Consumption.get_hts_off_peak(consumption.hts_off_peak),
+                htl_peak: Consumption.get_htl_peak(consumption.htl_peak),
+                htl_off_peak: Consumption.get_htl_off_peak(consumption.htl_off_peak),
+                eht_peak: Consumption.get_eht_peak(consumption.eht_peak),
+                eht_off_peak: Consumption.get_eht_off_peak(consumption.eht_off_peak),
                 details: details)
       total_info[:consumption_count] += 1
       total_info[:account_count] += count
-      total_info[:lt_peak] += consumption.lt_peak.nil? ? 0 : consumption.lt_peak
-      total_info[:lt_off_peak] += consumption.lt_off_peak.nil? ? 0 : consumption.lt_off_peak
-      total_info[:hts_peak] += consumption.hts_peak.nil? ? 0 : consumption.hts_peak
-      total_info[:hts_off_peak] += consumption.hts_off_peak.nil? ? 0 : consumption.hts_off_peak
-      total_info[:htl_peak] += consumption.htl_peak.nil? ? 0 : consumption.htl_peak
-      total_info[:htl_off_peak] += consumption.htl_off_peak.nil? ? 0 : consumption.htl_off_peak
-      total_info[:eht_peak] += consumption.eht_peak.nil? ? 0 : consumption.eht_peak
-      total_info[:eht_off_peak] += consumption.eht_off_peak.nil? ? 0 : consumption.eht_off_peak
+      total_info[:lt_peak] += Consumption.get_lt_peak(consumption.lt_peak)
+      total_info[:lt_off_peak] += Consumption.get_lt_off_peak(consumption.lt_off_peak)
+      total_info[:hts_peak] += Consumption.get_hts_peak(consumption.hts_peak)
+      total_info[:hts_off_peak] += Consumption.get_hts_off_peak(consumption.hts_off_peak)
+      total_info[:htl_peak] += Consumption.get_htl_peak(consumption.htl_peak)
+      total_info[:htl_off_peak] += Consumption.get_htl_off_peak(consumption.htl_off_peak)
+      total_info[:eht_peak] += Consumption.get_eht_peak(consumption.eht_peak)
+      total_info[:eht_off_peak] += Consumption.get_eht_off_peak(consumption.eht_off_peak)
     end
     render json: { list: data, total_info: total_info }, status: 200
   end
@@ -51,14 +52,14 @@ class Api::ConsumptionsController < Api::BaseController
              company_name: consumption.user.company_name,
              name: consumption.user.name,
              count: count,
-             lt_peak: consumption.lt_peak.nil? ? 0 : consumption.lt_peak,
-             lt_off_peak: consumption.lt_off_peak.nil? ? 0 : consumption.lt_off_peak,
-             hts_peak: consumption.hts_peak.nil? ? 0 : consumption.hts_peak,
-             hts_off_peak: consumption.hts_off_peak.nil? ? 0 : consumption.hts_off_peak,
-             htl_peak: consumption.htl_peak.nil? ? 0 : consumption.htl_peak,
-             htl_off_peak: consumption.htl_off_peak.nil? ? 0 : consumption.htl_off_peak,
-             eht_peak: consumption.eht_peak.nil? ? 0 : consumption.eht_peak,
-             eht_off_peak: consumption.eht_off_peak.nil? ? 0 : consumption.eht_off_peak,
+             lt_peak: Consumption.get_lt_peak(consumption.lt_peak),
+             lt_off_peak: Consumption.get_lt_off_peak(consumption.lt_off_peak),
+             hts_peak: Consumption.get_hts_peak(consumption.hts_peak),
+             hts_off_peak: Consumption.get_hts_off_peak(consumption.hts_off_peak),
+             htl_peak: Consumption.get_htl_peak(consumption.htl_peak),
+             htl_off_peak: Consumption.get_htl_off_peak(consumption.htl_off_peak),
+             eht_peak: Consumption.get_eht_peak(consumption.eht_peak),
+             eht_off_peak: Consumption.get_eht_off_peak(consumption.eht_off_peak),
              details: details }
 
     render json: cons, status: 200
