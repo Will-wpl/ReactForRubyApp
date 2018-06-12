2ca1b5a532cb87bcc93a6665abcfdae9c0965153
will <Will.wang@chinasofti.com>
2018-06-12 12:04:36 +0800

    delete encodeURI

--- app/webpack/components/retailer/workflow/propose-deviations.jsx
+++ app/webpack/components/retailer/workflow/propose-deviations.jsx
@@ -106,7 +106,7 @@ export class Proposedeviations extends React.Component{
         let withdraw = this.state.deviations_list;
         withdraw[obj.index].sp_response_status = '4';
         this.setState({deviations_list:withdraw});
-        retailerWithdraw(this.props.current.current.arrangement_id,{id:obj.id,propose_deviation:decodeURI(this.state.deviations_list[obj.index].propose_deviation)}).then(res=>{
+        retailerWithdraw(this.props.current.current.arrangement_id,{id:obj.id,propose_deviation:this.state.deviations_list[obj.index].propose_deviation}).then(res=>{
             this.refs.Modal.showModal();
             this.setState({
                 text:"You have successfully updated"
