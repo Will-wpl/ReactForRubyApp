c5f899f7da28158bb7ef83d49c3316301e3680bf
liuziyuan <liuziyuan.room@gmail.com>
2018-07-05 12:28:56 +0800

    completed workflow of  multiple and single tender workflow, will complete content of each flow

--- app/workflows/single_buyer_workflow.rb
+++ app/workflows/single_buyer_workflow.rb
@@ -2,7 +2,7 @@ require 'engine/workflow'
 require 'engine/node'
 require 'engine/event'
 
-class SingleBuyerWorkflow < Workflow
+class SingleBuyerWorkflow < BaseTenderWorkflow
 
   def initialize
     # from 1 is admin , 2 is retailer
@@ -13,7 +13,7 @@ class SingleBuyerWorkflow < Workflow
                       accept_all: Event.new(:accept_all, :node5, 2, 2, '0'),
                       propose_deviations: Event.new(:propose_deviations, :node3, 2, 2, '0'))
     @node3 = Node.new(:node3, 3, 'in processing',
-                      withdraw_all_deviations: Event.new(:withdraw_all_deviations, :node4, 2, 2, '0'),
+                      withdraw_all_deviations: Event.new(:withdraw_all_deviations, :node5, 2, 2, '0'),
                       submit_deviations: Event.new(:submit_deviations, :node3, 1, 2, '2'),
                       next: Event.new(:next, :node5, 2, 2, '0'),
                       send_response: Event.new(:send_response, :node3, 2, 1, '2'))
