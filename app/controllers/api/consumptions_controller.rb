3f3789ab39320d8a0f6a442210f253f60cbfc19a
liuziyuan <liuziyuan.room@gmail.com>
2018-07-12 16:13:14 +0800

    check consumption has contract_duration

--- app/controllers/api/consumptions_controller.rb
+++ app/controllers/api/consumptions_controller.rb
@@ -3,6 +3,9 @@ class Api::ConsumptionsController < Api::BaseController
 
   def index
     consumptions = Consumption.find_by_user_consumer_type(params[:consumer_type]).find_by_auction_id(params[:id]).is_participation
+    unless params[:contract_duration].blank?
+      consumptions.where('consumptions = ?', params[:contract_duration])
+    end
     consumptions = (params[:consumer_type] == '2') ? consumptions.order('users.company_name asc') : consumptions.order('users.name asc')
     data = []
     total_info = { consumption_count: 0, account_count: 0, lt_peak: 0, lt_off_peak: 0,
