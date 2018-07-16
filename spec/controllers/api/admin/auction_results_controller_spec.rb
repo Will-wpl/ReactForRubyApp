85d9f2fc7d23cdf4b784ea13070c743bfffda34d
liuziyuan <liuziyuan.room@gmail.com>
2018-07-16 18:14:58 +0800

    completed award duration function and ut

--- spec/controllers/api/admin/auction_results_controller_spec.rb
+++ spec/controllers/api/admin/auction_results_controller_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe Api::Admin::AuctionResultsController, type: :controller do
 
     end
 
-    describe 'contract_duration Index' do
+    describe 'contract duration Index' do
       let!(:auction1) { create(:auction, :for_next_month, :upcoming, :published, :started) }
       let!(:result1) { create(:auction_result, auction: auction1) }
       let!(:consumption) { create(:consumption, :init, user: buyer_user, auction: auction1, participation_status: '1', acknowledge: '1', contract_duration: '6' ) }
@@ -80,6 +80,23 @@ RSpec.describe Api::Admin::AuctionResultsController, type: :controller do
       context 'Pager auction result list' do
         def do_request
           get :award, params: { id: auction.id }
+        end
+
+        before { do_request }
+        it 'Success' do
+          expect(response).to have_http_status(:ok)
+          array = JSON.parse(response.body)
+          expect(array.size).to eq(1)
+          expect(array[0]['acknowledge']).to eq('1')
+        end
+      end
+
+      context 'Pager contract duration auction result list' do
+
+        let!(:auction1) { create(:auction, :for_next_month, :upcoming, :published, :started) }
+        let!(:consumption) { create(:consumption, :init, user: buyer_user, auction: auction1, participation_status: '1', acknowledge: '1', contract_duration: '6' ) }
+        def do_request
+          get :award, params: { id: auction1.id , contract_duration: '6' }
         end
 
         before { do_request }
