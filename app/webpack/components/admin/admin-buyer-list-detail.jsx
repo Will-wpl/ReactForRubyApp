0b2166ffde7f805a8eae7b60f786c8091046c319
Brian <yangqingxin83@163.com>
2018-07-17 15:08:38 +0800

    add approve and reject method for "View Consumption Details" page

--- app/webpack/components/admin/admin-buyer-list-detail.jsx
+++ app/webpack/components/admin/admin-buyer-list-detail.jsx
@@ -3,67 +3,157 @@ import ReactDOM from 'react-dom';
 import moment from 'moment';
 import AdminComsumptionList from './admin_shared/admin-comsumption-list';
 import AdminComsumptionPrice from './admin_shared/admin-comsumption-price';
-import {getSearchType} from '../../javascripts/componentService/util';
-import {getAdminBuyerListDetails} from '../../javascripts/componentService/admin/service';
+import { getSearchType } from '../../javascripts/componentService/util';
+import { getAdminBuyerListDetails,approveConsumptions } from '../../javascripts/componentService/admin/service';
+import { Modal } from '../shared/show-modal';
+import { validateNum, validateEmail, validator_Object, validator_Array, setValidationFaild, setValidationPass, changeValidate } from '../../javascripts/componentService/util';
 export default class AdminBuyerListDetail extends Component {
-  constructor(props){
-    super(props);
-    this.pageType = getSearchType();
-    if(this.pageType.indexOf('Company')>0){
-        this.datail_type = '2';
-    }else{
-        this.datail_type = '3';
+    constructor(props) {
+        super(props);
+        this.pageType = getSearchType();
+        if (this.pageType.indexOf('Company') > 0) {
+            this.datail_type = '2';
+        } else {
+            this.datail_type = '3';
+        }
+        this.state = {
+            consumption_id: "",
+            comsumption_list: [],
+            price: {},
+            detail: [],
+            detail_index: 0,
+            text: "",
+            comment: ""
+        }
+        this.type = sessionStorage.getItem('comsumptiontype');
+        if (this.type === 'company') {
+            this.type = 'View Company Consumption Details'
+        }
+        this.validateItem = {
+            comment: { cate: 'required' }
+        }
     }
-    this.state={
-        comsumption_list:[],
-        price:{},
-        detail:[],
-        detail_index:0
-    }
-    this.type = sessionStorage.getItem('comsumptiontype');
-    if (this.type === 'company') {
-        this.type = 'View Company Consumption Details'
-    }
-}
 
-componentDidMount() {
-    getAdminBuyerListDetails(window.location.href.split("consumptions/")[1]).then(res=>{
-        console.log(res);
-        this.setState({
-            comsumption_list:[res],
+    componentDidMount() {
+        let id = window.location.href.split("consumptions/")[1];
+        getAdminBuyerListDetails(id).then(res => {
+            console.log(res);
+            this.setState({
+                consumption_id: id,
+                comsumption_list: [res]
+
+            })
+        }, error => {
+
         })
-    },error=>{
-
-    })
-}
-show_detail(index,consumption_id){
-    let obj = $("#comsumption_list_table_"+index);
-    if(obj.is(":visible")){
-        obj.prev().removeClass("open");
-        obj.slideUp(300);
-    }else{
-        obj.prev().addClass("open");
-        obj.slideDown(300);
     }
-}
-render() {
-    return (
-        <div className="u-grid mg0">
-            <h2 className="u-mt2 u-mb2">View Consumption Details</h2>
-            <div className="col-sm-12 u-mb3">
-                <AdminComsumptionList visible="visible" comsumption_list={this.state.comsumption_list} detail={this.show_detail.bind(this)} type={this.type} />
+    show_detail(index, consumption_id) {
+        let obj = $("#comsumption_list_table_" + index);
+        if (obj.is(":visible")) {
+            obj.prev().removeClass("open");
+            obj.slideUp(300);
+        } else {
+            obj.prev().addClass("open");
+            obj.slideDown(300);
+        }
+    }
+    Change(type, e) {
+        let itemValue = e.target.value;
+        if (type === 'comment') {
+            this.setState({
+                comment: itemValue
+            })
+        }
+    }
+    checkRejectAction() { //when admin reject the request 
+        let flag = true;
+        let arr = validator_Object(this.state, this.validateItem);
+        if (arr) {
+            arr.map((item, index) => {
+                let column = item.column;
+                let cate = item.cate;
+                setValidationFaild(column, cate)
+            })
+        }
+        $('.validate_message').find('div').each(function () {
+            let className = $(this).attr('class');
+            if (className === 'errormessage') {
+                flag = false;
+                return false;
+            }
+        })
+        this.refs.Modal_Option.closeModal();
+        return flag;
+    }
+    judgeAction(type) {
+        if (type === 'reject') {
+            this.setState({
+                text: 'Are you sure you want to reject the request?',
+            }, () => {
+                this.refs.Modal_Option.showModal('comfirm', { action: 'reject' }, '');
+            });
+        }
+        else {
+            this.setState({ text: "Are you sure you want to approve the request?" });
+            this.refs.Modal_Option.showModal('comfirm', { action: 'approve' }, '');
+        }
+    }
+    doAction(obj) {
+        let param = {
+            consumption_id: this.state.consumption_id,
+            approved: obj.action === 'reject' ? "" : 1,
+            comment: this.state.comment
+        };
+        if (obj.action === 'reject') {
+            if (this.checkRejectAction()) {
+                approveConsumptions(param).then(res => {
+                    location.href = "/admin/auctions/published";
+                })
+            }
+        }
+        else {
+            approveConsumptions(param).then(res => {
+                location.href = "/admin/auctions/published";
+            })
+        }
+    }
+    render() {
+        return (
+            <div className="u-grid mg0 validate_message">
+                <h2 className="u-mt2 u-mb2">View Consumption Details</h2>
+                <div className="col-sm-12 u-mb3">
+                    <AdminComsumptionList visible="visible" comsumption_list={this.state.comsumption_list} detail={this.show_detail.bind(this)} type={this.type} />
+                </div>
+                <div className="col-sm-12 u-mb3">
+                    <div>
+                        <div className="lm--formItem lm--formItem--inline string">
+                            <label className="lm--formItem-left lm--formItem-label string required">
+                                Comment:
+                                    </label>
+                            <div className="lm--formItem-right lm--formItem-control">
+                                <textarea name="comment" value={this.state.comment} onChange={this.Change.bind(this, 'comment')} ref="comment" aria-required="true"></textarea>
+                                <div className='isPassValidate' id='comment_message' >This field is required!</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="retailer_btn">
+                    <button id="save_form" className="lm--button lm--button--primary" onClick={this.judgeAction.bind(this, 'reject')}>Reject</button>
+                    <button id="submit_form" className="lm--button lm--button--primary" onClick={this.judgeAction.bind(this, 'approve')}>Approve</button>
+                </div>
+                <div className="createRaMain u-grid">
+                    <a className="lm--button lm--button--primary u-mt3" href="javascript:javascript:self.location=document.referrer;" >Back</a>
+                </div>
+                <Modal acceptFunction={this.doAction.bind(this)} text={this.state.text} type={"comfirm"} ref="Modal_Option" />
             </div>
-            <div className="createRaMain u-grid">
-            <a className="lm--button lm--button--primary u-mt3" href="javascript:javascript:self.location=document.referrer;" >Back</a>
-            </div>
-        </div>
-    )
-  }
+
+        )
+    }
 }
 
 function run() {
     const domNode = document.getElementById('admin_buyer_list_detail');
-    if(domNode !== null){
+    if (domNode !== null) {
         ReactDOM.render(
             React.createElement(AdminBuyerListDetail),
             domNode
