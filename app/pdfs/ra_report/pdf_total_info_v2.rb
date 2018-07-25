54635945a0129480e27f8a6d7ac48d17b63799a4
Brian <yangqingxin83@163.com>
2018-07-25 17:09:42 +0800

    get total volume.

--- app/pdfs/ra_report/pdf_total_info_v2.rb
+++ app/pdfs/ra_report/pdf_total_info_v2.rb
@@ -2,4 +2,8 @@ class PdfTotalInfoV2 < PdfTotalInfo
   def get_contract_period_end_date(param)
     (param[:auction_result].contract_period_end_date).strftime("%-d %b %Y")
   end
+
+  def get_total_volume(param)
+    PdfUtils.number_helper.number_to_currency(param[:auction_result].total_volume, precision: 0, unit: '')
+  end
 end
