98b768bbebe215899868a20b3f9090e566e992e4
will <Will.wang@chinasofti.com>
2018-07-27 14:12:47 +0800

    readonly for past

--- app/webpack/components/retailer/retailer-before-live.jsx
+++ app/webpack/components/retailer/retailer-before-live.jsx
@@ -90,7 +90,6 @@ export class RetailerBeforeLive extends Component {
     getAuctionTime(auctionId) {
         getAuctionTimeRule(auctionId).then(res => {
             const isOver = this.isCountDownOver(res[AUCTION_PROPS.ACTUAL_BEGIN_TIME], res[AUCTION_PROPS.ACTUAL_CURRENT_TIME]);
-            console.log("离开始前时间：",parseInt(moment(res[AUCTION_PROPS.ACTUAL_BEGIN_TIME]).add(parseInt(0-this.props.auction.starting_price_time),"hours") - moment(res[AUCTION_PROPS.ACTUAL_CURRENT_TIME])))
             if(parseInt(moment(res[AUCTION_PROPS.ACTUAL_BEGIN_TIME]).add(parseInt(0-this.props.auction.starting_price_time),"hours") - moment(res[AUCTION_PROPS.ACTUAL_CURRENT_TIME]))<=0){
                 this.setState({timeShow:true});
             }else{
