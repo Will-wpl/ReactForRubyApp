1df668158e609628f9799b6fd12e0cca4aa0e9b2
Brian <yangqingxin83@163.com>
2018-08-03 11:22:54 +0800

    add context

--- app/webpack/components/retailer/workflow/tender-documents.jsx
+++ app/webpack/components/retailer/workflow/tender-documents.jsx
@@ -109,7 +109,6 @@ export class Tenderdocuments extends React.Component {
                 <div className="lm--formItem lm--formItem--inline string u-mt3 role_select">
                     <label className="lm--formItem-left lm--formItem-label string required">
                         Electricity Procurement Agreement:
-                <span>{(this.props.single != 5 && this.props.single != 4) ? "You are bounded by the Electricity Procurement Agreement. Please click 'Proceed' to continue." : ""}</span>
                     </label>
                     <div className="lm--formItem-right lm--formItem-control">
                         <ul className="tender_list">
@@ -119,18 +118,23 @@ export class Tenderdocuments extends React.Component {
                         </ul>
                     </div>
                 </div>
+                <div className="lm--formItem lm--formItem--inline string u-mt3 role_select">
+                    <label className=" lm--formItem-label ">
+                        {(this.props.single != 5 && this.props.single != 4) ? "You are bounded by the Electricity Procurement Agreement. Please click 'Proceed' to continue." : ""}
+                    </label>
+                </div>
                 {this.props.single === 5 ? (this.props.current.actions ?
-                    <div className="workflow_btn u-mt3">
-                        <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_propose_deviations)} onClick={this.showConfirm.bind(this, 'Propose_Deviations')} className="lm--button lm--button--primary">Propose Deviations</button>
-                        <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_accept_all)} onClick={this.showConfirm.bind(this, 'Accept_All')} className="lm--button lm--button--primary">Accept All</button>
-                    </div> : <div></div>) : (this.props.single === 4 ? <div className="workflow_btn u-mt3">
-                        <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_propose_deviations)} onClick={this.showConfirm.bind(this, 'Propose_Deviations')} className="lm--button lm--button--primary">Propose Deviations</button>
-                        <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_accept_all)} onClick={this.showConfirm.bind(this, 'Accept_All')} className="lm--button lm--button--primary">Accept & Proceed</button>
-                    </div> : <div className="workflow_btn u-mt3">
-                            <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_accept_all)} onClick={this.showConfirm.bind(this, 'Accept_All')} className="lm--button lm--button--primary">Proceed</button>
-                        </div>)}
-                <Modal text={this.state.text} acceptFunction={this.state.buttonType === 'Propose_Deviations' ? this.propose_deviations.bind(this) : this.accept_all.bind(this)} ref="Modal" />
-            </div>
-        )
-    }
-}
+                        <div className="workflow_btn u-mt3">
+                            <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_propose_deviations)} onClick={this.showConfirm.bind(this, 'Propose_Deviations')} className="lm--button lm--button--primary">Propose Deviations</button>
+                            <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_accept_all)} onClick={this.showConfirm.bind(this, 'Accept_All')} className="lm--button lm--button--primary">Accept All</button>
+                        </div> : <div></div>) : (this.props.single === 4 ? <div className="workflow_btn u-mt3">
+                            <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_propose_deviations)} onClick={this.showConfirm.bind(this, 'Propose_Deviations')} className="lm--button lm--button--primary">Propose Deviations</button>
+                            <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_accept_all)} onClick={this.showConfirm.bind(this, 'Accept_All')} className="lm--button lm--button--primary">Accept & Proceed</button>
+                        </div> : <div className="workflow_btn u-mt3">
+                                <button disabled={this.props.propsdisabled ? true : (!this.props.current.actions.node2_retailer_accept_all)} onClick={this.showConfirm.bind(this, 'Accept_All')} className="lm--button lm--button--primary">Proceed</button>
+                            </div>)}
+                    <Modal text={this.state.text} acceptFunction={this.state.buttonType === 'Propose_Deviations' ? this.propose_deviations.bind(this) : this.accept_all.bind(this)} ref="Modal" />
+                </div>
+                )
+            }
+        }
