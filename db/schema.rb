672a70290bb82dc68b09a63aeaf42444f6848c3c
liuziyuan <liuziyuan.room@gmail.com>
2018-08-10 13:01:03 +0800

    add approval datatime to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810035630) do
+ActiveRecord::Schema.define(version: 20180810045942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -417,6 +417,7 @@ ActiveRecord::Schema.define(version: 20180810035630) do
     t.string "changed_contract"
     t.bigint "entity_id"
     t.bigint "tenant_id"
+    t.datetime "approval_date_time"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
