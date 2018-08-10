1f5ea65b783308e9747f24d4779f37158c0850c7
liuziyuan <liuziyuan.room@gmail.com>
2018-08-10 17:59:00 +0800

    add field for logic change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810045942) do
+ActiveRecord::Schema.define(version: 20180810094011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -249,6 +249,7 @@ ActiveRecord::Schema.define(version: 20180810045942) do
     t.bigint "user_id"
     t.integer "is_default"
     t.string "approval_status"
+    t.bigint "user_entity_id"
     t.index ["user_id"], name: "index_company_buyer_entities_on_user_id"
   end
 
@@ -361,6 +362,7 @@ ActiveRecord::Schema.define(version: 20180810045942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "consumption_detail_id"
     t.index ["user_id"], name: "index_user_attachments_on_user_id"
   end
 
