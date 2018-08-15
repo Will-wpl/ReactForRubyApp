590b21ae880578d8315ef32f9670a46ec7d1ee44
Brian <yangqingxin83@163.com>
2018-08-15 16:08:46 +0800

    modify the upload component sytle

--- app/webpack/components/admin/admin-email-template.jsx
+++ app/webpack/components/admin/admin-email-template.jsx
@@ -1,50 +1,50 @@
-import React, {Component, PropTypes} from 'react'
+import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom';
-import {Modal} from '../shared/show-modal';
-import {getEmailList,getEmailListItem,getEmailItemUpdate,getEmailFile} from '../../javascripts/componentService/admin/service';
+import { Modal } from '../shared/show-modal';
+import { getEmailList, getEmailListItem, getEmailItemUpdate, getEmailFile } from '../../javascripts/componentService/admin/service';
 import { UploadFile } from '../shared/upload';
 export default class EmailTemplates extends Component {
     constructor(props) {
         super(props);
         this.state = {
             text: "",
-            email_list:[],
-            listdetail:{},
-            uploadUrl:'/api/admin/user_attachments?file_type=',
-            fileData:{
-                "LETTER_OF_AUTHORISATION":[
-                    {buttonName:"none",files:[]}
+            email_list: [],
+            listdetail: {},
+            uploadUrl: '/api/admin/user_attachments?file_type=',
+            fileData: {
+                "LETTER_OF_AUTHORISATION": [
+                    { buttonName: "none", files: [] }
                 ]
             }
         }
     }
-    componentDidMount(){
-        getEmailList().then(res=>{
-            this.setState({email_list:res});
-        },error=>{
+    componentDidMount() {
+        getEmailList().then(res => {
+            this.setState({ email_list: res });
+        }, error => {
 
         })
-        getEmailFile('LETTER_OF_AUTHORISATION').then(res=>{
+        getEmailFile('LETTER_OF_AUTHORISATION').then(res => {
             let file = this.state.fileData;
             file.LETTER_OF_AUTHORISATION[0].files = res;
             this.setState({
-                fileData:file
+                fileData: file
             })
         })
     }
-    showEmail(id){
-        getEmailListItem(id).then(res=>{
-            this.setState({listdetail:res,text:''});
-            this.refs.Modal.showModal('comfirm',res,'email_template');
+    showEmail(id) {
+        getEmailListItem(id).then(res => {
+            this.setState({ listdetail: res, text: '' });
+            this.refs.Modal.showModal('comfirm', res, 'email_template');
         })
     }
-    changeEmail(obj){
-        getEmailItemUpdate(obj).then(res=>{
-            this.setState({text:"Email Update Successful!"})
-            setTimeout(()=>{
+    changeEmail(obj) {
+        getEmailItemUpdate(obj).then(res => {
+            this.setState({ text: "Email Update Successful!" })
+            setTimeout(() => {
                 this.refs.Modal.showModal();
-            },200)
-        },error=>{
+            }, 200)
+        }, error => {
 
         })
     }
@@ -58,26 +58,26 @@ export default class EmailTemplates extends Component {
                         <div className="table-head">
                             <table className="retailer_fill">
                                 <thead>
-                                <tr>
-                                    <th width={"69.7%"}>Name</th>
-                                    <th>Email</th>
-                                </tr>
+                                    <tr>
+                                        <th width={"69.7%"}>Name</th>
+                                        <th>Email</th>
+                                    </tr>
                                 </thead>
                             </table>
                         </div>
                         <div className="table-body">
                             <table className="retailer_fill">
                                 <tbody>
-                                {
-                                    this.state.email_list.map((item, index) => {
-                                        return (
-                                            <tr key={index}>
-                                                <td width={"70%"}>{item.subject}</td>
-                                                <td><a onClick={this.showEmail.bind(this,item.id)} className={"edit"}>edit</a></td>
-                                            </tr>
-                                        )
-                                    })
-                                }
+                                    {
+                                        this.state.email_list.map((item, index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td width={"70%"}>{item.subject}</td>
+                                                    <td><a onClick={this.showEmail.bind(this, item.id)} className={"edit"}>edit</a></td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
                                 </tbody>
                             </table>
                         </div>
@@ -86,12 +86,12 @@ export default class EmailTemplates extends Component {
                                 <abbr title="required">*</abbr> Letter Of Authorisation :
                             </label>
                             <div className="lm--formItem-right lm--formItem-control u-grid mg0">
-                                <UploadFile type="LETTER_OF_AUTHORISATION" required="required" showList="1" col_width="12"  showWay="1" fileData={this.state.fileData.LETTER_OF_AUTHORISATION} propsdisabled={false}  uploadUrl={this.state.uploadUrl}/>
+                                <UploadFile type="LETTER_OF_AUTHORISATION" required="required" deleteType="admin" showList="1" col_width="10" showWay="1" fileData={this.state.fileData.LETTER_OF_AUTHORISATION} propsdisabled={false} uploadUrl={this.state.uploadUrl} />
                             </div>
                         </div>
                     </div>
                 </div>
-                <Modal formSize="middle" text={this.state.text} acceptFunction={this.changeEmail.bind(this)} listdetail={this.state.listdetail} listdetailtype="Email Template"  ref="Modal" />
+                <Modal formSize="middle" text={this.state.text} acceptFunction={this.changeEmail.bind(this)} listdetail={this.state.listdetail} listdetailtype="Email Template" ref="Modal" />
             </div>
 
         )
@@ -100,7 +100,7 @@ export default class EmailTemplates extends Component {
 
 function run() {
     const domNode = document.getElementById('admin_email_template');
-    if(domNode !== null){
+    if (domNode !== null) {
         ReactDOM.render(
             React.createElement(EmailTemplates),
             domNode
--- app/webpack/components/shared/upload.jsx
+++ app/webpack/components/shared/upload.jsx
@@ -42,7 +42,7 @@ export class UploadFile extends React.Component {
                                 <div className="progress-bar" style={{ width: "0%" }}>0%</div>
                             </div>
                             <div className="progress_files">
-                                <div id="showMessage" className={this.props.validate ? 'isPassValidate' : 'errormessage'}>This field is required!</div>
+                                {/* <div id="showMessage" className={this.props.validate ? 'isPassValidate' : 'errormessage'}>This field is required!</div> */}
                                 {this.state.showList == 1 ?
                                     <ul>
                                         {
