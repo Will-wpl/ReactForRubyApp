fe150dc7e660949921703f7cd0c3940e236b9448
Brian <yangqingxin83@163.com>
2018-08-16 13:53:02 +0800

    modify  peak/off-peak's calculate value

--- app/webpack/components/admin/admin_shared/admin-comsumption-list.jsx
+++ app/webpack/components/admin/admin_shared/admin-comsumption-list.jsx
@@ -95,8 +95,8 @@ export default class AdminComsumptionList extends Component {
                                                     <td>{it.blk_or_unit} {it.street} {it.unit_number} {it.postal_code} </td>
                                                     <td className="left">
                                                         <div><span>Total Monthly: </span><span className="textDecoration" >{parseInt(it.totals)}</span><span> kWh/month</span></div>
-                                                        <div><span>Peak: </span><span><span>{it.peak} kWh/month </span>({parseFloat(it.peak_pct).toFixed(2)}%</span>)<span style={{ fontWeight: "bold", fontSize: "14px" }} title="Off Peak is auto calculated by 1-Peak." >&nbsp;&nbsp;?</span></div>
-                                                        <div><span>Off-Peak: </span><span>{it.off_peak} kWh/month </span><span>({parseFloat(100 - it.peak_pct).toFixed(2)}%)</span></div>
+                                                        <div><span>Peak: </span><span><span>{parseInt(it.peak)} kWh/month </span>({parseFloat(it.peak_pct).toFixed(2)}%</span>)<span style={{ fontWeight: "bold", fontSize: "14px" }} title="Off Peak is auto calculated by 1-Peak." >&nbsp;&nbsp;?</span></div>
+                                                        <div><span>Off-Peak: </span><span>{parseInt(it.off_peak)} kWh/month </span><span>({parseFloat(100 - it.peak_pct).toFixed(2)}%)</span></div>
                                                         <div className={it.user_attachment ? "isDisplay" : "isHide"}><span>Upload bill(s):</span>
                                                         <span>
                                                             <ul className="attachementList">
