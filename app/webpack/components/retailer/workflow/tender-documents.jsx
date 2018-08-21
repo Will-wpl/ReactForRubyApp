3d4a0c37c1c13206ad18191b09d1dd272bb7c855
Brian <yangqingxin83@163.com>
2018-08-21 16:07:28 +0800

    modify message

--- app/webpack/components/retailer/workflow/tender-documents.jsx
+++ app/webpack/components/retailer/workflow/tender-documents.jsx
@@ -44,7 +44,8 @@ export class Tenderdocuments extends React.Component {
         } else {
             this.refs.Modal.showModal("comfirm");
             this.setState({
-                text: "Are you sure you want to participate in the auction? By clicking 'Yes', you confirm your participation in the auction and are bounded by the Retailer Platform Terms of Use. Please be reminded that you will not be allowed to withdraw your participation."
+                // text: "Are you sure you want to participate in the auction? By clicking 'Yes', you confirm your participation in the auction and are bounded by the Retailer Platform Terms of Use. Please be reminded that you will not be allowed to withdraw your participation."
+                text:"Are you sure you want to participate in the auction?<br>By clicking 'Yes', you confirm your participation in the auction and will not be allowed to withdraw your participation"
             });
         }
     }
