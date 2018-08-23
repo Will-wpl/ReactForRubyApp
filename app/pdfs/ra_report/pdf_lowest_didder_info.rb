2cd50923cbacbf2ce77b28b789ab50027a77d2af
Brian <yangqingxin83@163.com>
2018-08-23 15:06:09 +0800

    Modify admin RA report export pdf.

--- app/pdfs/ra_report/pdf_lowest_didder_info.rb
+++ app/pdfs/ra_report/pdf_lowest_didder_info.rb
@@ -25,10 +25,11 @@ class PdfLowestDidderInfo
       lowest_price_bidder = auction_result.lowest_price_bidder
       lowest_average_price = PdfUtils.number_helper.number_to_currency(auction_result.lowest_average_price.to_f, precision: 4, unit: '$ ', format: '%u%n/kWh')
     end
-    bidder_table = [["<font size='18'><color rgb='#{status_color}'>Status: #{status}</color></font>"],
-                    [bidder_text],
-                    ["#{bidder_text2}: #{lowest_price_bidder}"],
-                    ["#{bidder_text3}: #{lowest_average_price}"]]
-    pdf.table(bidder_table, :cell_style => {:inline_format => true, :width => pdf.bounds.right, :border_width => 0})
+    data = [["<font size='18'><color rgb='#{status_color}'>Status: #{status}</color></font>"]]
+    data.push(["Justification: #{auction_result.justification}"]) unless auction_result.justification.blank?
+    data.push([bidder_text])
+    data.push(["#{bidder_text2}: #{lowest_price_bidder}"])
+    data.push(["#{bidder_text3}: #{lowest_average_price}"])
+    pdf.table(data, :cell_style => {:inline_format => true, :width => pdf.bounds.right, :border_width => 0})
   end
 end
