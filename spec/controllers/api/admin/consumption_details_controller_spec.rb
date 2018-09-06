f4b69706d24db4739fca25ed8d42fe883f70a1b4
liuziyuan <liuziyuan.room@gmail.com>
2018-09-06 13:39:56 +0800

    Fixed test spec

--- spec/controllers/api/admin/consumption_details_controller_spec.rb
+++ spec/controllers/api/admin/consumption_details_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Api::Admin::ConsumptionDetailsController, type: :controller do
         hash = JSON.parse(response.body)
         expect(hash.size).to eq(9)
         expect(hash['consumption_details'].size).to eq(4)
-        expect(hash['consumption_details'][0]['intake_level']).to eq('LT')
+        expect(hash['consumption_details'][0]['intake_level']).to eq('EHT')
         expect(response).to have_http_status(:ok)
       end
     end
