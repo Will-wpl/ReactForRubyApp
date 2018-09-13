188a7e7eb674e2e00404d01eae1806cfd2782c35
will <Will.wang@chinasofti.com>
2018-09-13 12:55:37 +0800

    fix modal middle

--- app/webpack/components/admin/admin-common-template.jsx
+++ app/webpack/components/admin/admin-common-template.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom';
+import { removeUserAttachFile} from '../../javascripts/componentService/admin/service';
 import { Modal } from '../shared/show-modal';
 import { getEmailFile } from '../../javascripts/componentService/admin/service';
 import { UploadFile } from '../shared/upload';
@@ -26,6 +27,18 @@ export default class CommonTemplates extends Component {
             })
         })
     }
+    remove_file(typeindex, fileindex, fileid) {
+            let fileObj;
+            removeUserAttachFile(fileid).then(res => {
+                fileObj = this.state.fileData;
+                fileObj['COMMON'][typeindex].files.splice(fileindex, 1);
+                this.setState({
+                    fileData: fileObj
+                })
+            }, error => {
+
+            })
+    }
     render() {
         //console.log('ranking', this.props.ranking)
         return (
@@ -39,6 +52,22 @@ export default class CommonTemplates extends Component {
                             <UploadFile type="COMMON" required="required" deleteType="userAttach" showList="1" col_width="10" showWay="1" fileData={this.state.fileData.COMMON} propsdisabled={false} uploadUrl={this.state.uploadUrl} />
                         </div>
                     </div>
+                    <table className={"retailer_fill common_template"}>
+                        <thead>
+                        <tr><th width={"25%"}>File Name</th><th>File Url</th><th>Operation</th></tr>
+                        </thead>
+                        <tbody>
+                        {this.state.fileData.COMMON[0].files.map((item,index)=>{
+                            return <tr key={index}>
+                                <td><a target="_blank" download={item.file_name} href={item.file_path}>{item.file_name}</a></td>
+                                <td>{item.file_path}</td>
+                                <td>
+                                    <button className="lm--button lm--button--primary">Copy</button>
+                                    <button className="lm--button lm--button--primary" onClick={this.remove_file.bind(this, 0, index, item.id)}>Delete</button>
+                                </td></tr>
+                        })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
