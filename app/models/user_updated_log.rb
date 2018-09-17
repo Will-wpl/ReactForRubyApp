41b64101c334eca3a1643d0549e78fe08cfbc5f9
liuziyuan <liuziyuan.room@gmail.com>
2018-09-17 14:42:30 +0800

    Fixed user reject status

--- app/models/user_updated_log.rb
+++ app/models/user_updated_log.rb
@@ -1,5 +1,5 @@
 class UserUpdatedLog < ApplicationRecord
 
   # Scopes
-  scope :find_by_user_id, ->(user_id) { where('users_id = ?', user_id).order(created_at: :desc) }
+  scope :find_by_user_id, ->(user_id) { where('users_id = ?', user_id).order(updated_at: :desc) }
 end
