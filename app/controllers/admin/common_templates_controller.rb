20a737472e49a2c3365fa7fe25bd590595ab6111
liuziyuan <liuziyuan.room@gmail.com>
2018-09-21 10:49:28 +0800

    add common template

---
+++ app/controllers/admin/common_templates_controller.rb
@@ -0,0 +1,4 @@
+class Admin::CommonTemplatesController < Admin::BaseController
+  def index
+  end
+end
---
+++ spec/controllers/admin/common_template_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Admin::CommonTemplateController, type: :controller do
+
+end
