457e73b60cdd3948a597dbb1bb34722026c1e2f6
Brian <yangqingxin83@163.com>
2018-09-25 09:42:44 +0800

    modify reset function for buyer and retailer manage page

--- app/webpack/javascripts/componentService/common/searchConfig.js
+++ app/webpack/javascripts/componentService/common/searchConfig.js
@@ -1,128 +1,128 @@
 import React, { Component, PropTypes } from 'react'
 export const searchTypeData = {
-    "Retailer List":{//connect back end OK
-        list_url:"/api/admin/users/retailers",
-        list_data:[
-                    {title:"Company Name:",type:"company_name",species:"input",operator:'like'},
-                    {title:"License Number:",type:"company_license_number",species:"input",operator:'like'},
-                    {title:"Status:",type:"approval_status",operator:'=',table:'users',species:"select",options:[{option:"All",value:""},{option:"Rejected",value:"0"},{option:"Approved",value:"1"},{option:"Pending",value:"2"}]}
-                ]
-    },	
-    
-    "Buyer List":{//connect back end OK
-        list_url:"/api/admin/users/buyers",
-        list_data:[
-            {title:"Name:",type:"name",species:"input",operator:'like',table:'users'},
-            {title:"Email:",type:"email",species:"input",operator:'like'},
-            {title:"Consumer Type:",type:"consumer_type",operator:'=',species:"select",options:[{option:"Company",value:"2"},{option:"Individual",value:"3"}]},
-            {title:"Status:",type:"approval_status",operator:'=',table:'users',species:"select",options:[{option:"All",value:""},{option:"Rejected",value:"0"},{option:"Approved",value:"1"},{option:"Pending",value:"2"},{option:"Registering",value:"3"}]}
+    "Retailer List": {//connect back end OK
+        list_url: "/api/admin/users/retailers",
+        list_data: [
+            { title: "Company Name:", type: "company_name", species: "input", operator: 'like' },
+            { title: "License Number:", type: "company_license_number", species: "input", operator: 'like' },
+            { title: "Status:", type: "approval_status", operator: '=', table: 'users', species: "select", options: [{ option: "All", value: "" }, { option: "Registering", value: "3" }, { option: "Pending", value: "2" }, { option: "Approved", value: "1" }, { option: "Rejected", value: "0" },] }
         ]
     },
-    "Deleted Retailer List":{//connect back end OK
-        list_url:"/api/admin/users/retailers_deleted",
-        list_data:[
-            {title:"Company Name:",type:"company_name",species:"input",operator:'like'},
-            {title:"License Number:",type:"company_license_number",species:"input",operator:'like'}
+
+    "Buyer List": {//connect back end OK
+        list_url: "/api/admin/users/buyers",
+        list_data: [
+            { title: "Name:", type: "name", species: "input", operator: 'like', table: 'users' },
+            { title: "Email:", type: "email", species: "input", operator: 'like' },
+            { title: "Consumer Type:", type: "consumer_type", operator: '=', species: "select", options: [{ option: "Company", value: "2" }, { option: "Individual", value: "3" }] },
+            { title: "Status:", type: "approval_status", operator: '=', table: 'users', species: "select", options: [{ option: "All", value: "" }, { option: "Registering", value: "3" }, { option: "Pending", value: "2" }, { option: "Approved", value: "1" }, { option: "Rejected", value: "0" }] }
         ]
     },
-    "Deleted Buyer List":{//connect back end OK
-        list_url:"/api/admin/users/buyers_deleted",
-        list_data:[
-            {title:"Name:",type:"name",species:"input",operator:'like',table:'users'},
-            {title:"Email:",type:"email",species:"input",operator:'like'},
-            {title:"Consumer Type:",type:"consumer_type",operator:'=',species:"select",options:[{option:"Company",value:"2"},{option:"Individual",value:"3"}]}
+    "Deleted Retailer List": {//connect back end OK
+        list_url: "/api/admin/users/retailers_deleted",
+        list_data: [
+            { title: "Company Name:", type: "company_name", species: "input", operator: 'like' },
+            { title: "License Number:", type: "company_license_number", species: "input", operator: 'like' }
         ]
     },
-    "Unpublished Auction List":{//connect back end OK
-        list_url:"/api/admin/auctions/unpublished",
-        list_data:[
-            {title:"Name:",type:"name",species:"input",operator:'like'},
-            {title:"Date:",type:"start_datetime",species:"datePacker",operator:'date_between'}
+    "Deleted Buyer List": {//connect back end OK
+        list_url: "/api/admin/users/buyers_deleted",
+        list_data: [
+            { title: "Name:", type: "name", species: "input", operator: 'like', table: 'users' },
+            { title: "Email:", type: "email", species: "input", operator: 'like' },
+            { title: "Consumer Type:", type: "consumer_type", operator: '=', species: "select", options: [{ option: "Company", value: "2" }, { option: "Individual", value: "3" }] }
         ]
     },
-    "Published Auction List":{//connect back end OK
-        list_url:"/api/admin/auctions/published",
-        list_data:[
-            {title:"ID:",type:"published_gid",species:"input",operator:'like',table:'auctions'},
-            {title:"Name:",type:"name",species:"input",operator:'like',table:'auctions'},
-            {title:"Date:",type:"start_datetime",species:"datePacker",operator:'date_between',table:'auctions'}
+    "Unpublished Auction List": {//connect back end OK
+        list_url: "/api/admin/auctions/unpublished",
+        list_data: [
+            { title: "Name:", type: "name", species: "input", operator: 'like' },
+            { title: "Date:", type: "start_datetime", species: "datePacker", operator: 'date_between' }
         ]
     },
-    "Retailer Published Auction List":{
-        list_url:"/api/retailer/auctions/published",
-        list_data:[
-            {title:"ID:",type:"published_gid",species:"input",operator:'like',table:'auctions'},
-            {title:"Name:",type:"name",species:"input",operator:'like',table:'auctions'},
-            {title:"Date:",type:"start_datetime",species:"datePacker",operator:'date_between',table:'auctions'}
+    "Published Auction List": {//connect back end OK
+        list_url: "/api/admin/auctions/published",
+        list_data: [
+            { title: "ID:", type: "published_gid", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Name:", type: "name", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Date:", type: "start_datetime", species: "datePacker", operator: 'date_between', table: 'auctions' }
         ]
     },
-    "Status of Participation":{
-        list_url:"/api/buyer/auctions/published",
-        list_data:[
-            {title:"Name:",type:"name",species:"input",operator:'like',table:'auctions'},
-            {title:"Date:",type:"start_datetime",species:"datePacker",operator:'date_between',table:'auctions'},
-            {title:"Auctions Status:",type:"publish_status",species:"select",operator:'=',table:'auctions',options:[{option:"All",value:""},{option:"Unpublished",value:"0"},{option:"Published",value:"1"}]},
-            {title:"Participation Status:",type:"participation_status",species:"select",operator:'=',options:[{option:"All",value:""},{option:"Rejected",value:"0"},{option:"Confirmed",value:"1"},{option:"Pending",value:"2"}]}
+    "Retailer Published Auction List": {
+        list_url: "/api/retailer/auctions/published",
+        list_data: [
+            { title: "ID:", type: "published_gid", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Name:", type: "name", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Date:", type: "start_datetime", species: "datePacker", operator: 'date_between', table: 'auctions' }
         ]
     },
-    "Select Retailers":{
-        list_url:"/api/admin/auctions/"+sessionStorage.auction_id+"/retailers",
-        list_data:[
-            {title:"Company Name:",type:"company_name",species:"input",operator:'like'},
-            {title:"Status:",type:"status",species:"select",operator:'=',options:[{option:"All",value:""},{option:"Not Invited",value:"0"},{option:"Pending Notification",value:"2"},{option:"Notification Sent",value:"3"},{option:"Invited",value:"1"}]}
+    "Status of Participation": {
+        list_url: "/api/buyer/auctions/published",
+        list_data: [
+            { title: "Name:", type: "name", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Date:", type: "start_datetime", species: "datePacker", operator: 'date_between', table: 'auctions' },
+            { title: "Auctions Status:", type: "publish_status", species: "select", operator: '=', table: 'auctions', options: [{ option: "All", value: "" }, { option: "Unpublished", value: "0" }, { option: "Published", value: "1" }] },
+            { title: "Participation Status:", type: "participation_status", species: "select", operator: '=', options: [{ option: "All", value: "" }, { option: "Rejected", value: "0" }, { option: "Confirmed", value: "1" }, { option: "Pending", value: "2" }] }
         ]
     },
-    "Select Company Buyers":{
-        list_url:"/api/admin/auctions/"+sessionStorage.auction_id+"/buyers",
-        list_data:[
-            {title:"Company Name:",type:"company_name",species:"input",operator:'like',table:'users'},
-            {title:"Status:",type:"status",species:"select",operator:'=',options:[{option:"All",value:""},{option:"Not Invited",value:"0"},{option:"Pending Notification",value:"2"},{option:"Notification Sent",value:"3"},{option:"Invited",value:"1"}]},
-            {title:"consumer_type:",type:"consumer_type",operator:'=',defaultval:'2',species:'hidden'}
+    "Select Retailers": {
+        list_url: "/api/admin/auctions/" + sessionStorage.auction_id + "/retailers",
+        list_data: [
+            { title: "Company Name:", type: "company_name", species: "input", operator: 'like' },
+            { title: "Status:", type: "status", species: "select", operator: '=', options: [{ option: "All", value: "" }, { option: "Not Invited", value: "0" }, { option: "Pending Notification", value: "2" }, { option: "Notification Sent", value: "3" }, { option: "Invited", value: "1" }] }
         ]
     },
-    "Select Individual Buyers":{
-        list_url:"/api/admin/auctions/"+sessionStorage.auction_id+"/buyers",
-        list_data:[
-            {title:"Name:",type:"name",species:"input",operator:'like',table:'users'},
-            {title:"Housing Type:",type:"account_housing_type",species:"select",operator:'=',options:[{option:"All",value:""},{option:"HDB",value:"0"},{option:"Private High-rise",value:"1"},{option:"Landed",value:"2"}]},
-            {title:"Status:",type:"status",species:"select",operator:'=',options:[{option:"All",value:""},{option:"Not Invited",value:"0"},{option:"Pending Notification",value:"2"},{option:"Notification Sent",value:"3"},{option:"Invited",value:"1"}]},
-            {title:"consumer_type:",type:"consumer_type",operator:'=',defaultval:'3',species:'hidden'}
+    "Select Company Buyers": {
+        list_url: "/api/admin/auctions/" + sessionStorage.auction_id + "/buyers",
+        list_data: [
+            { title: "Company Name:", type: "company_name", species: "input", operator: 'like', table: 'users' },
+            { title: "Status:", type: "status", species: "select", operator: '=', options: [{ option: "All", value: "" }, { option: "Not Invited", value: "0" }, { option: "Pending Notification", value: "2" }, { option: "Notification Sent", value: "3" }, { option: "Invited", value: "1" }] },
+            { title: "consumer_type:", type: "consumer_type", operator: '=', defaultval: '2', species: 'hidden' }
         ]
     },
-    "Past Reverse Auction":{//connect back end OK
-        list_url:"/api/admin/auction_results",
-        list_data:[
-            {title:"ID:",type:"published_gid",species:"input",operator:'like',table:'auctions'},
-            {title:"Name:",type:"name",species:"input",operator:'like',table:'auctions'},
-            {title:"Date:",type:"start_datetime",species:"datePacker",operator:'date_between',table:'auctions'}
+    "Select Individual Buyers": {
+        list_url: "/api/admin/auctions/" + sessionStorage.auction_id + "/buyers",
+        list_data: [
+            { title: "Name:", type: "name", species: "input", operator: 'like', table: 'users' },
+            { title: "Housing Type:", type: "account_housing_type", species: "select", operator: '=', options: [{ option: "All", value: "" }, { option: "HDB", value: "0" }, { option: "Private High-rise", value: "1" }, { option: "Landed", value: "2" }] },
+            { title: "Status:", type: "status", species: "select", operator: '=', options: [{ option: "All", value: "" }, { option: "Not Invited", value: "0" }, { option: "Pending Notification", value: "2" }, { option: "Notification Sent", value: "3" }, { option: "Invited", value: "1" }] },
+            { title: "consumer_type:", type: "consumer_type", operator: '=', defaultval: '3', species: 'hidden' }
         ]
     },
-    "Retailer Past Reverse Auction":{//connect back end OK
-        list_url:"/api/retailer/auction_results",
-        list_data:[
-            {title:"ID:",type:"published_gid",species:"input",operator:'like',table:'auctions'},
-            {title:"Name:",type:"name",species:"input",operator:'like',table:'auctions'},
-            {title:"Date:",type:"start_datetime",species:"datePacker",operator:'date_between',table:'auctions'}
+    "Past Reverse Auction": {//connect back end OK
+        list_url: "/api/admin/auction_results",
+        list_data: [
+            { title: "ID:", type: "published_gid", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Name:", type: "name", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Date:", type: "start_datetime", species: "datePacker", operator: 'date_between', table: 'auctions' }
         ]
     },
-    "Buyer Past Reverse Auction":{//connect back end OK
-        list_url:"/api/buyer/auction_results",
-        list_data:[
-            {title:"ID:",type:"published_gid",species:"input",operator:'like',table:'auctions'},
-            {title:"Name:",type:"name",species:"input",operator:'like',table:'auctions'},
-            {title:"Date:",type:"start_datetime",species:"datePacker",operator:'date_between',table:'auctions'}
+    "Retailer Past Reverse Auction": {//connect back end OK
+        list_url: "/api/retailer/auction_results",
+        list_data: [
+            { title: "ID:", type: "published_gid", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Name:", type: "name", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Date:", type: "start_datetime", species: "datePacker", operator: 'date_between', table: 'auctions' }
         ]
     },
-    "User Extension List (Retailers)":{//connect back end OK
-        list_url:"/api/admin/user_extensions",
-        list_data:[
-             {title:"Company Name:",type:"company_name",species:"input",operator:'like'},
+    "Buyer Past Reverse Auction": {//connect back end OK
+        list_url: "/api/buyer/auction_results",
+        list_data: [
+            { title: "ID:", type: "published_gid", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Name:", type: "name", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Date:", type: "start_datetime", species: "datePacker", operator: 'date_between', table: 'auctions' }
         ]
     },
-    "Auction Log List":{
-        list_url:`/api/admin/auctions${window.location.href.indexOf('/log')>0?(window.location.href.split("/auctions")[1].split("/log")[0]):'1'}/log`,
-        list_data:[
-            {title:"Company Name:",type:"company_name",species:"input",operator:'like'},
+    "User Extension List (Retailers)": {//connect back end OK
+        list_url: "/api/admin/user_extensions",
+        list_data: [
+            { title: "Company Name:", type: "company_name", species: "input", operator: 'like' },
+        ]
+    },
+    "Auction Log List": {
+        list_url: `/api/admin/auctions${window.location.href.indexOf('/log') > 0 ? (window.location.href.split("/auctions")[1].split("/log")[0]) : '1'}/log`,
+        list_data: [
+            { title: "Company Name:", type: "company_name", species: "input", operator: 'like' },
         ]
     }
 }
