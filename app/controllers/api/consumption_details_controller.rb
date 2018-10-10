461b51b2a2a80729d93b77342afee5c916a904e4
liuziyuan <liuziyuan.room@gmail.com>
2018-10-10 09:35:57 +0800

    it contains pending buyer entities when participating consumption detail

--- app/controllers/api/consumption_details_controller.rb
+++ app/controllers/api/consumption_details_controller.rb
@@ -10,7 +10,7 @@ class Api::ConsumptionDetailsController < Api::BaseController
       auction = consumption.auction
       contract_duration = auction.auction_contracts.select('contract_duration').sort_by {|contract| contract.contract_duration.to_i}
       # buyer_entities = CompanyBuyerEntity.find_by_status_user(CompanyBuyerEntity::ApprovalStatusApproved, consumption.user_id).order(is_default: :desc)
-      buyer_entities = CompanyBuyerEntity.where('user_id = ? and approval_status in ?', consumption.user_id, [CompanyBuyerEntity::ApprovalStatusApproved , CompanyBuyerEntity::ApprovalStatusPending])
+      buyer_entities = CompanyBuyerEntity.where('user_id = ? and approval_status in (?)', consumption.user_id, [CompanyBuyerEntity::ApprovalStatusApproved , CompanyBuyerEntity::ApprovalStatusPending])
       if auction.auction_contracts.blank?
         buyer_revv_tc_attachment = AuctionAttachment.find_by(auction_id: consumption.auction_id, file_type: 'buyer_tc_upload')
         seller_buyer_tc_attachment = nil
