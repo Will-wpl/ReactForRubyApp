abdfd15b656d2ca06edc4e2e065878591f3f4f4a
mark liu <liuziyuan.room@gmail.com>
2018-11-15 10:37:34 +0800

    add tc_attach_info to auction

--- app/controllers/api/consumptions_controller.rb
+++ app/controllers/api/consumptions_controller.rb
@@ -90,6 +90,12 @@ class Api::ConsumptionsController < Api::BaseController
         render json: { message: 'consumption exist' }, status: 200
       else
         auction = Auction.find(params[:auction_id])
+        tc_attach_info = {}
+        tc_attach_info[:SELLER_BUYER_TC] = UserAttachment.find_last_by_type(UserAttachment::FileType_Seller_Buyer_TC).id
+        tc_attach_info[:SELLER_REVV_TC] = UserAttachment.find_last_by_type(UserAttachment::FileType_Seller_REVV_TC).id
+        tc_attach_info[:BUYER_REVV_TC] = UserAttachment.find_last_by_type(UserAttachment::FileType_Buyer_REVV_TC).id
+        auction.tc_attach_info = tc_attach_info
+        auction.save
         if auction.buyer_type == Auction::SingleBuyerType
           auction.consumptions.find_by_user_consumer_type(User::ConsumerTypeCompany).destroy_all
         end
