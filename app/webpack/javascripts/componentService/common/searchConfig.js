e2ac318518a3c6b1c936937d7ec85a24c8f9ef93
will <Will.wang@chinasofti.com>
2018-11-26 10:07:26 +0800

    add new UI image

--- app/webpack/javascripts/componentService/common/searchConfig.js
+++ app/webpack/javascripts/componentService/common/searchConfig.js
@@ -100,7 +100,7 @@ export const searchTypeData = {
     "Retailer Past Reverse Auction": {//connect back end OK
         list_url: "/api/retailer/auction_results",
         list_data: [
-            { title: "ID:", type: "published_gid", species: "input", operator: 'like', table: 'auctions' },
+            { title: "Reference ID:", type: "published_gid", species: "input", operator: 'like', table: 'auctions' },
             { title: "Name:", type: "name", species: "input", operator: 'like', table: 'auctions' },
             { title: "Date:", type: "start_datetime", species: "datePacker", operator: 'date_between', table: 'auctions' }
         ]
