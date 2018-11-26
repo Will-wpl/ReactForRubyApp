5c87935dd3016bac2470883c8c1861664fed6f91
will <Will.wang@chinasofti.com>
2018-11-26 21:47:33 +0800

    add new UI image

--- app/webpack/components/retailer/retailer-tc-upload-approve.jsx
+++ app/webpack/components/retailer/retailer-tc-upload-approve.jsx
@@ -114,7 +114,7 @@ export class RetailerTCUploadApprove extends Component {
                         <div id="chkBPT_message" className="isPassValidate" style={{ marginLeft: "33px" }}> Please check this box if you want to proceed.</div>
                     </div> : <div></div>}
                     <br/>
-                    <h4>*Note: The amended Terms & Conditions of Use (Retailer) will not be applicable to (I) upcoming auctions that you have already been invited to participate in, (ii) auctions that are in progress, and (iii) auctions that had already been completed.</h4>
+                    <h4>The amended {this.state.epaIsExist?"Electricity Purchase Contract":""} {this.state.bptIsExist && this.state.epaIsExist?"&":""} {this.state.epaIsExist?"Terms & Conditions of Use (Retailer)":""} will not be applicable to (i) upcoming auctions that you have already been invited to participate in, (ii) auctions that are in progress, and (iii) auctions that had already been completed.</h4>
                     <br/>
                 </div>
                 <div className="col-sm-12 col-md-12 u-grid  btnProceed">
