369ce17f7c42886fb5f5da95a06d395242b07c1e
liuziyuan <liuziyuan.room@gmail.com>
2018-11-28 13:26:05 +0800

    request auction logics

--- app/controllers/api/request_auctions_controller.rb
+++ app/controllers/api/request_auctions_controller.rb
@@ -37,13 +37,9 @@ class Api::RequestAuctionsController < Api::BaseController
     # save request auction
     if request_auction.save!
       # save attachment
-      file = params[:file]
-      unless file.blank?
-        uploader = upload_file
-        attachment = RequestAttachment.new
-        attachment.file_name = file.original_filename #filename
-        attachment.file_type = params[:file_type]
-        attachment.file_path = uploader.url
+      attachment_id = params[:attachment_id]
+      unless attachment_id.blank?
+        attachment = RequestAttachment.find(attachment_id)
         attachment.request_auction_id = request_auction.id
         attachment.save!
       end
@@ -93,15 +89,4 @@ class Api::RequestAuctionsController < Api::BaseController
     render json: { buyer_entity_contracts: buyer_entity_contracts }, status: 200
   end
 
-  private
-  def upload_file
-    file = params[:file]
-    mounted_as = []
-    mounted_as.push(current_user.id.to_s) unless current_user&.has_role?(:admin)
-    mounted_as.push(Time.current.to_f.to_s.delete('.'))
-
-    uploader = AvatarUploader.new(RequestAttachment, mounted_as)
-    uploader.store!(file)
-    uploader
-  end
 end
