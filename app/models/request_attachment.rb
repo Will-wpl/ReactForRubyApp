deaec5db8815889c309c9ee7b31929ed281bd74b
mark liu <liuziyuan.room@gmail.com>
2018-11-30 16:09:12 +0800

    add tc

--- app/models/request_attachment.rb
+++ app/models/request_attachment.rb
@@ -12,7 +12,7 @@ class RequestAttachment < ApplicationRecord
   # Validations
 
   # Scopes
-
+  scope :find_by_type_request, ->(file_type, request_id) { where('file_type = ? and request_auction_id = ?', file_type, request_id) }
   # Callbacks
 
   # Delegates
@@ -20,4 +20,25 @@ class RequestAttachment < ApplicationRecord
   # Custom
 
   # Methods (class methods before instance methods)
+
+  def self.has_attachment(file_type, auction_id)
+    request = Auction.find(auction_id).request_auction
+    if request.nil?
+      false
+    else
+      request_attachment = RequestAttachment.find_by_type_request(file_type, request.id).order(updated_at: :desc).first
+      if request_attachment.nil?
+        false
+      else
+        true
+      end
+    end
+  end
+
+  def self.find_last_by_type_request(file_type, request_id)
+    request = Auction.find(auction_id).request_auction
+    unless request.nil?
+      RequestAttachment.find_by_type_request(file_type, request.id).order(updated_at: :desc).first
+    end
+  end
 end
