e34dda03dd5d609049f7d22b618f72c546ab9e8e
mark liu <liuziyuan.room@gmail.com>
2018-12-04 13:47:35 +0800

    modify

--- app/controllers/api/buyer/tenders_controller.rb
+++ app/controllers/api/buyer/tenders_controller.rb
@@ -1,4 +1,4 @@
-class Api::Admin::TendersController < Api::TendersController
+class Api::Buyer::TendersController < Api::TendersController
   before_action :buyer_required
   before_action :set_arrangement, only: %i[node3_admin node4_admin_accept node4_admin_reject node4_admin]
 end
