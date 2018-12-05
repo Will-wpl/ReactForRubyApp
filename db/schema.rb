1a7674d8df7dc62ff654c2acf0e66a21c7737dcb
liuziyuan <liuziyuan.room@gmail.com>
2018-12-05 19:12:23 +0800

    fixed issue -- add column called fexible to request_auctions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205062331) do
+ActiveRecord::Schema.define(version: 20181205111019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -363,6 +363,7 @@ ActiveRecord::Schema.define(version: 20181205062331) do
     t.bigint "total_volume"
     t.bigint "auction_id"
     t.datetime "accept_date_time"
+    t.string "fexible"
     t.index ["user_id"], name: "index_request_auctions_on_user_id"
   end
 
