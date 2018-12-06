940e912e878022898d6db7468e293b4ec00dcdf3
liuziyuan <liuziyuan.room@gmail.com>
2018-12-06 12:38:17 +0800

    fixed issue

--- app/controllers/api/admin/request_auctions_controller.rb
+++ app/controllers/api/admin/request_auctions_controller.rb
@@ -5,10 +5,10 @@ class Api::Admin::RequestAuctionsController < Api::RequestAuctionsController
     if params.key?(:page_size) && params.key?(:page_index)
       search_params = reject_params(params, %w[controller action sort_by])
       search_where_array = set_search_params(search_params)
-      result = RequestAuction.all.where(search_where_array)
+      result = RequestAuction.includes(:user).all.where(search_where_array)
       total = result.count
     else
-      result = RequestAuction.all
+      result = RequestAuction.includes(:user).all
       total = result.count
     end
     result = result.page(params[:page_index]).per(params[:page_size])
@@ -18,7 +18,7 @@ class Api::Admin::RequestAuctionsController < Api::RequestAuctionsController
       results = get_order_list(result, params, headers)
       results.each do |result|
         data.push(id: result.id, name: result.name, duration: result.duration,
-                  buyer_name: result.user.company_name,
+                  company_name: result.user.company_name,
                   contract_period_start_date: result.contract_period_start_date,
                   buyer_type: (result.buyer_type == RequestAuction::SingleBuyerType)? 'Single':'Multiple',
                   allow_deviation: (result.allow_deviation == RequestAuction::AllowDeviation)? 'Yes':'No',
@@ -40,7 +40,7 @@ private
 def get_request_auction_headers1
   [
       { name: 'Name', field_name: 'name', table_name: 'request_auctions' },
-      { name: 'Buyer Name', field_name: 'buyer_name', table_name: 'users' },
+      { name: 'Buyer Name', field_name: 'company_name', table_name: 'users' },
       # { name: 'Contract Duration', field_name: 'duration', table_name: 'request_auctions' },
       { name: 'Start Date', field_name: 'contract_period_start_date', table_name: 'request_auctions' },
       { name: 'Single / Multiple Buyer[s]', field_name: 'buyer_type', table_name: 'request_auctions' }
