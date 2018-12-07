97f5123562ec87ba5f928495835eba86297fefb4
mark liu <liuziyuan.room@gmail.com>
2018-12-07 14:07:59 +0800

    modify

--- app/workflows/requested_single_buyer_workflow.rb
+++ app/workflows/requested_single_buyer_workflow.rb
@@ -2,7 +2,7 @@ require 'engine/workflow'
 require 'engine/node'
 require 'engine/event'
 
-class RequestedSingleBuyerWorkflow < SingleBuyerWorkflow
+class RequestedSingleBuyerWorkflow < BaseTenderWorkflow
 
   def initialize
     # from 1 is admin , 2 is retailer, 3 is buyer
@@ -22,6 +22,34 @@ class RequestedSingleBuyerWorkflow < SingleBuyerWorkflow
     @node5 = Node.new(:node5, 5, 'end',
                       submit: Event.new(:submit, nil, 1, 2, nil))
     super(node1: @node1, node2: @node2, node3: @node3, node5: @node5)
+  end
+
+  def get_current_action_status(arrangement_id)
+    sm = TenderStateMachine.where(arrangement_id: arrangement_id).last
+    if node1?(sm)
+      { node1_retailer_accept: true }
+    elsif node2?(sm)
+      { node2_retailer_accept_all: true, node2_retailer_propose_deviations: true }
+    elsif node3_retailer?(sm)
+      if node3_retailer_next?(sm)
+        { node3_retailer_next: true}
+      else
+        back = node3_retailer_has_submit?(sm) ? {node3_retailer_back: false} : {node3_retailer_back: true}
+        if node3_retailer_has_chats?(sm)
+          back.merge!({ node3_retailer_withdraw_all_deviations: true, node3_retailer_submit_deviations: true, node3_retailer_save: true })
+        else
+          back.merge!({ node3_retailer_submit_deviations: true, node3_retailer_save: true })
+        end
+      end
+    elsif node3_admin?(sm)
+      { node3_send_response: true }
+    elsif node3_buyer?(sm)
+      { node3_send_response: true }
+    elsif node5?(sm)
+      { node5_retailer_submit: true }
+    else
+      {}
+    end
   end
   
 end
