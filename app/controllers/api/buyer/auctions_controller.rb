a7ef79152ae473daac194a289e29a579fac106ac
mark liu <liuziyuan.room@gmail.com>
2018-12-07 14:31:31 +0800

    modify

--- app/controllers/api/buyer/auctions_controller.rb
+++ app/controllers/api/buyer/auctions_controller.rb
@@ -5,6 +5,7 @@ class Api::Buyer::AuctionsController < Api::AuctionsController
     if params[:id].nil?
       render json: nil
     else
+      auction = Auction.find(params[:id])
       if Arrangement.auction_of_current_user(auction.id, current_user.id).exists?
         render json: { id: auction.id, publish_status: auction.publish_status }, status: 200
       elsif current_user.id == auction.request_owner_id
