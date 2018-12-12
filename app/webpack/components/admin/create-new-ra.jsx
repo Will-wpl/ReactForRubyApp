5b23093ea8bb88e909edecbc570b4671cfdb7f69
justin <justin.guo@chinasofti.com>
2018-12-12 09:34:45 +0800

    modify the upload label

--- app/webpack/components/admin/create-new-ra.jsx
+++ app/webpack/components/admin/create-new-ra.jsx
@@ -970,9 +970,9 @@ export class CreateNewRA extends Component {
                                 {
                                     this.state.tc_name.length ? <dd className="lm--formItem lm--formItem--inline string optional">
                                         <span className="lm--formItem-left lm--formItem-label string optional">
-                                            <abbr title="required"></abbr>T&C :</span>
-                                        <label className="lm--formItem-right lm--formItem-control" style={{marginTop:"10px",paddingLeft:"-25px"}}>
-                                            <a href={this.state.tc_path} download={this.state.tc_path}>{this.state.tc_name}</a>
+                                            <abbr title="required"></abbr>Electricity Purchase Contract :</span>
+                                        <label className="lm--formItem-right lm--formItem-control" style={{ marginTop: "10px", paddingLeft: "-125px" }}>
+                                            <div> <a href={this.state.tc_path} download={this.state.tc_path}>{this.state.tc_name}</a></div>
                                         </label>
                                     </dd> : ""
                                 }
