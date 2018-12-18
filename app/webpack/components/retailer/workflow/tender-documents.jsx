cd6742a126e557bbf712dca8582664080bfcd4e8
will <Will.wang@chinasofti.com>
2018-12-18 13:46:31 +0800

    add buyer workflow

--- app/webpack/components/retailer/workflow/tender-documents.jsx
+++ app/webpack/components/retailer/workflow/tender-documents.jsx
@@ -146,7 +146,7 @@ export class Tenderdocuments extends React.Component {
                                 Option 1: Check the box below and click on 'Accept & Participate' if you do not wish to propose deviation(s) to the Electricity Purchase Contract.
                             </label>
                         </div>
-                        <div className="lm--formItem lm--formItem--inline string u-mt1">
+                        <div className="lm--formItem lm--formItem--inline string">
                             <label className=" lm--formItem-label ">
                                 Option 2: Click on 'Propose Deviations' if you wish to propose deviation(s) to the Electricity Purchase Contract.
                             </label>
