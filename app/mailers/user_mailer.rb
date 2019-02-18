b66faa4cd5fc2feabe4fd83259f772bdf771fc46
justin.guo <justin.guo@chinasofti.com>
2019-02-18 16:12:46 +0800

    Add comment field.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -84,7 +84,7 @@ class UserMailer < ApplicationMailer
   def buyer_participate_rejected(user, param)
     mail_template = get_template('28')
     email_subject = mail_template.subject.gsub(/#name_of_ra/, param[:name_of_ra].to_s).gsub(/#date_time/, param[:date_time])
-    email_body = mail_template.body.gsub(/#buyer_company_name/, user.company_name).gsub(/#name_of_ra/, param[:name_of_ra].to_s).gsub(/#date_time/, param[:date_time]).gsub(/#comment/, param[:comment].to_s)
+    email_body = mail_template.body.gsub(/#buyer_company_name/, user.company_name).gsub(/#name_of_ra/, param[:name_of_ra].to_s).gsub(/#date_time/, param[:date_time]).gsub(/#comment/, nl2br(CGI.unescape(param[:comment].to_s)))
     send_email(user.email, email_body, email_subject)
   end
 
@@ -135,7 +135,7 @@ class UserMailer < ApplicationMailer
                    end
     mail_template = get_template('32')
     email_subject = mail_template.subject
-    email_body = mail_template.body.gsub(/#buyer_company_name/, user.company_name).gsub(/#respond/, respond_text.to_s).gsub(/#comment/, comment_text.to_s)
+    email_body = mail_template.body.gsub(/#buyer_company_name/, user.company_name).gsub(/#respond/, respond_text.to_s).gsub(/#comment/, nl2br(CGI.unescape(comment_text.to_s)))
     send_email(user.email, email_body, email_subject)
   end
 
